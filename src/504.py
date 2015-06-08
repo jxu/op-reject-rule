c47c0f7a440fc1b201c8cef71a351790d87099f8
jxu <7989982+jxu@users.noreply.github.com>
2015-06-07 21:16:58 -0400

    504 more optimize

--- src/504.py
+++ src/504.py
@@ -1,12 +1,14 @@
 # Pick's theorem: A = i + b/2 - 1
-# 4 triangles: 73.9 s   Upper-lower triangles: 20.0 s
+# 4 triangles: 73.9 s
+# Upper-lower triangles: 20.0 s
+# a,c and b,d symmetry: 6.5 s (third possible)
 from fractions import gcd
 import time
 
 def lattice(m):
     square_num = set(i**2 for i in range(int(20000**0.5)))
 
-    # Generate all triangle values at start
+    # Precalculate triangle values
     tri_table = [[[0]*101 for i in range(101)] for j in range(101)]
     for a in range(101):
         for h in range(101):
@@ -17,12 +19,16 @@ def lattice(m):
 
     s = 0
     for a in range(1, m+1):
-        for c in range(1, m+1):
+        for c in range(1, a+1):
+            r = 1
+            if c != a: r *= 2 # a,c symmetry
             for b in range(1, m+1):
                 # Upper triangle + horizontal edge correction
                 abc_ = tri_table[a][b][c] + a + c - 1
-                for d in range(1, m+1):
-                    if abc_ + tri_table[a][d][c] in square_num: s += 1
+                for d in range(1, b+1):
+                    if abc_ + tri_table[a][d][c] in square_num:
+                        if d != b: s += 2*r # b,d symmetry
+                        else: s += r
 
     return s
 

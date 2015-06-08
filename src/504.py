46623fee76f529d373291f14804971acd81a75d3
jxu <7989982+jxu@users.noreply.github.com>
2015-06-07 20:54:26 -0400

    504 optimize

--- src/504.py
+++ src/504.py
@@ -1,27 +1,28 @@
 # Pick's theorem: A = i + b/2 - 1
+# 4 triangles: 73.9 s   Upper-lower triangles: 20.0 s
 from fractions import gcd
 import time
 
 def lattice(m):
-    square_num = set()
-    for i in range(int(20000**0.5)): square_num.add(i**2)
+    square_num = set(i**2 for i in range(int(20000**0.5)))
 
-    # Generate all triangle values at start (50% faster)
-    tri_table = [[-1]*101 for i in range(101)]
+    # Generate all triangle values at start
+    tri_table = [[[0]*101 for i in range(101)] for j in range(101)]
     for a in range(101):
-        for b in range(101):
-            A = a*b/2
-            B = gcd(a, b) + a + b
-            tri_table[a][b] = A - B/2 + 1
+        for h in range(101):
+            for c in range(101):
+                A = (a+c)*h/2
+                B = gcd(a, h) + gcd(h, c) + a + c
+                tri_table[a][h][c] = A - B/2 + 1
 
     s = 0
     for a in range(1, m+1):
-        for b in range(1, m+1):
-            for c in range(1, m+1):
+        for c in range(1, m+1):
+            for b in range(1, m+1):
+                # Upper triangle + horizontal edge correction
+                abc_ = tri_table[a][b][c] + a + c - 1
                 for d in range(1, m+1):
-                    # Combine triangles
-                    i = tri_table[a][b] + tri_table[b][c] + tri_table[c][d] + tri_table[d][a] + (a + b + c + d - 4) + 1
-                    if i in square_num: s += 1
+                    if abc_ + tri_table[a][d][c] in square_num: s += 1
 
     return s
 

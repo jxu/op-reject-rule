ae4d336f984926f5e887dd93632160e7d763ca6a
jxu <7989982+jxu@users.noreply.github.com>
2015-06-13 02:17:59 -0400

    50 new

--- src/050.py
+++ src/050.py
@@ -1,23 +1,20 @@
+# Bounded sum generation
+# Solutions for 10^n starting with n=2: 41, 953, 9521, 92951, 997651, 9951191, 99819619
 import number
 primes = number.sieve(10**6)
+sp = set(primes)
 
 max_l = best_p = 0
-for i in range(len(primes)-1, 0, -1): # Larger numbers have longer sequences
-    if primes[i] < 9*10**5: break
+for start in range(10): # Long sequences will start with low primes (test first 10)
+    s = l = 0
+    for i in range(start, len(primes)):
+        l += 1
+        s += primes[i]
+        if s >= 10**6: break
 
-    for j in range(10): # Starting prime
-        s = l = 0
-        for k in range(j, len(primes)):
-            s += primes[k]
-            l += 1
-            if s >= primes[i]: break
-
-        if s == primes[i]:
-            print(s, primes[j], l)
-            if l > max_l:
-                max_l = l
-                best_p = s
-
-            break
+        if s in sp and l > max_l:
+            best_p = max(best_p, s)
+            q = primes[start]
+            max_l = l
 
 print(best_p)

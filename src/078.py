4bb063cd30e72e20b7fdb768bae703517febfb68
jxu <7989982+jxu@users.noreply.github.com>
2015-07-15 00:47:46 -0400

    78 optimize

---
+++ src/078.py
@@ -0,0 +1,31 @@
+N_MAX = 100000
+memo = [0]*N_MAX
+memo[0] = 1
+
+pent = [0]*2*N_MAX
+for k in range(N_MAX):
+    pent[k] = k*(3*k - 1)//2
+    pent[-k] = -k*(3*-k - 1)//2
+
+
+def p(n):
+    if n < 0: return 0
+    if memo[n] != 0:
+        return memo[n]
+
+    s = 0
+    k = 1
+    while pent[k] <= n:
+        s += (-1)**(k-1) * p(n - pent[k])
+        s += (-1)**(k-1) * p(n - pent[-k])
+        k += 1
+
+
+    memo[n] = s
+    return s
+
+for n in range(N_MAX):
+    if p(n) % 10**6 == 0:
+        print(n)
+        break
+

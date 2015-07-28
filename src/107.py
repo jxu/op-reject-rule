75faba357accb64699c1345b2703ff11e6828621
jxu <7989982+jxu@users.noreply.github.com>
2015-07-28 01:58:19 -0400

    107

--- src/107.py
+++ src/107.py
@@ -1,4 +1,4 @@
-INF = 10**8
+# Prim's algorithm
 total_edge = 0
 
 with open("p107_network.txt", 'r') as f:
@@ -6,52 +6,23 @@ with open("p107_network.txt", 'r') as f:
     for i in range(40):
         adj[i] = adj[i].split(',')
         adj[i] = [None if n == '-' else int(n) for n in adj[i]]
-        total_edge += sum(n for n in adj[i] if n)
+        total_edge += sum(n for n in adj[i] if n)  # Each edge counted twice
+
+T = set()
+X = set([0])  # Starting point
+
+while len(X) != 40:
+    crossing = set()
+
+    for x in X:
+        for k in range(40):
+            if k not in X and adj[x][k]:
+                crossing.add((x, k))
+
+    edge = sorted(crossing, key=lambda e: adj[e[0]][e[1]])[0]
+    T.add(edge)
+    X.add(edge[1])
 
 
-def min_key(key, mst):
-    min_ = INF
-    min_index = None
-
-    for v in range(40):
-        if not mst[v] and key[v] < min_:
-            min_ = key[v]
-            min_index = v
-
-    return min_index
-
-
-def prim_mst(graph):
-    parent = [None]*40
-    key = [INF]*40
-    mst = [False]*40
-
-    key[0] = 0
-    parent[0] = -1
-
-    for i in range(40-1):
-        u = min_key(key, mst)
-        mst[u] = True
-
-        for v in range(40):
-            if graph[u][v] and mst[v] == False and graph[u][v] < key[v]:
-                parent[v] = u
-                key[v] = graph[u][v]
-
-    for i in range(1, 40):
-        print(parent[i], i, graph[i][parent[i]])
-
-    return key
-
-
-print(total_edge - sum(prim_mst(adj)))
-
-
-
-
-
-
-
-
-
-
+min_weight = sum(adj[e[0]][e[1]] for e in T)
+print(total_edge//2 - min_weight)

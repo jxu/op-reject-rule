63c5f9f88df6a7f422bbe957bec9e72131f8547c
jxu <7989982+jxu@users.noreply.github.com>
2015-08-22 23:16:34 -0400

    75 shorten

--- src/075.py
+++ src/075.py
@@ -7,11 +7,9 @@ triangles = [0]*(L+1)
 for m in range(1, int((L/2)**0.5) + 1):
     for n in range(m%2 + 1, m, 2):  # m-n odd; m,n > 0 
         s = 2*m**2 + 2*m*n
-        if s > L:
-            break
-            
+
         if gcd(m, n) == 1:
-            for k in range(1, int(L/s)+1):
+            for k in range(1, int(L/s)+1):  # No k if s > L
                 triangles[k*s] += 1
-                
+
 print(triangles.count(1))

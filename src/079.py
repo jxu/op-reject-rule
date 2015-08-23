3a0f3b8658c0ac403ba75cb68e5f99e3638e7a88
jxu <7989982+jxu@users.noreply.github.com>
2015-08-23 00:36:52 -0400

    79

--- src/079.py
+++ src/079.py
@@ -1,15 +1,38 @@
-# BFS
+# Topological Sort
 with open("p079_keylog.txt") as f:
     logins = f.read().split('\n')[:-1]
     
 logins = set(logins)
-   
-passcodes = [(l, {l}) for l in logins]
 
-while True:
-    new_passcodes = []
-    for p in passcodes:
-        print(p)
+
+adj_list = set()
+digits = set()
+for l in logins:
+    adj_list.add((l[0], l[1]))
+    adj_list.add((l[1], l[2]))
+    for c in l:
+        digits.add(c)
+
+s = ""
     
+while adj_list:
+    rem = digits.copy()
+    for a in adj_list:
+        try: 
+            rem.remove(a[1])
+        except:
+            pass
+        
+    digits.remove(list(rem)[0])
+    s += list(rem)[0]
     
-    break
+    new_adj_list = set()
+    
+    for a in adj_list:
+        if a[0] != list(rem)[0]:
+            new_adj_list.add(a)
+            
+            
+    adj_list = new_adj_list
+            
+print(s + '0')

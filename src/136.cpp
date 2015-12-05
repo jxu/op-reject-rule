36fad65e86964a288f06efa98c96ed16ac48c893
jxu <7989982+jxu@users.noreply.github.com>
2015-12-05 03:01:28 -0500

    136

---
+++ src/136.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+const int MAX_N = 50000000;
+int d[MAX_N+1];
+
+int main()
+{
+    for (int y=1; y<=MAX_N; y++)
+    {
+        for (int a=y/4; a<y && y*(4*a-y) <= MAX_N; a++)
+        {
+            if (4*a-y > 0) d[y*(4*a-y)]++;
+        }
+    }
+    std::cout << std::count(d, d+MAX_N+1, 1) << std::endl;
+    return 0;
+}
--- src/136.py
+++ src/136.py
@@ -2,12 +2,13 @@
 def f(max_n):
     d = [0]*(max_n+1)
     for y in range(1, max_n+1):
-        for a in range(1, y):
+        if y%100 == 0: print(y)
+        for a in range(y//4, y):  # Skip entire neg range
             x = y*(4*a-y)
-            if x > max_n: break
-            if x > 0: d[y*(4*a-y)] += 1
+            if x > 0:
+                if x > max_n: break
+                d[y*(4*a-y)] += 1
 
     return d
 
-print(f(5000000).count(1))
-
+print(f(50000000).count(1))

5d4a9377ff11f6f325f5aa09aff4cabd6b946e5a
jxu <7989982+jxu@users.noreply.github.com>
2016-03-03 01:32:38 -0500

    136.py changes

--- src/136.py
+++ src/136.py
@@ -2,12 +2,12 @@
 def f(max_n):
     d = [0]*(max_n+1)
     for y in range(1, max_n+1):
-        if y%100 == 0: print(y)
+        if y%100000 == 0: print(y)
         for a in range(y//4, y):  # Skip entire neg range
             x = y*(4*a-y)
+            if x > max_n: break
             if x > 0:
-                if x > max_n: break
-                d[y*(4*a-y)] += 1
+                d[x] += 1
 
     return d
 

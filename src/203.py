4d735e629adc8ae8e5d12d751cfac239ff56dba3
jxu <7989982+jxu@users.noreply.github.com>
2016-04-13 10:02:16 -0400

    203

---
+++ src/203.py
@@ -0,0 +1,20 @@
+from number import sieve, combination
+
+primes = sieve(int(combination(50, 25)**0.5)) # Max prime
+prime_squares = [p**2 for p in primes]
+square_free = set()
+
+def is_square_free(c):
+    for ps in prime_squares:
+        if ps > c: return True
+        if c % ps == 0: return False
+    print("error")
+
+for n in range(0, 51):
+    for k in range(0, n//2 + 1):
+        c = combination(n, k)
+        if is_square_free(c): square_free.add(c)
+
+print(sum(square_free))
+
+

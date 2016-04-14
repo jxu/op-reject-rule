b1f13de990fcc9501a487010f3f64f68c38c445a
jxu <7989982+jxu@users.noreply.github.com>
2016-04-14 00:56:48 -0400

    351

--- src/351.py
+++ src/351.py
@@ -1,3 +1,5 @@
 # Hidden points are coordinates (either one-dimensional or two-dimensional (x,y)) that are not coprime
 # H(n) = 6(Sum[n - phi(n), {n, 1, 5}])
 from number import totient_sum_simple # Until fixed
+def H(n): return 6*(n*(n+1)//2 - totient_sum_simple(n))
+print(H(100000000))

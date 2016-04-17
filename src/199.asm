b5284d162cc76d09a896d9df50d51d2c29d2367a
jxu <7989982+jxu@users.noreply.github.com>
2016-04-16 23:08:09 -0400

    199.asm main calculations

--- src/199.asm
+++ src/199.asm
@@ -1,39 +1,58 @@
 # Translation of python code because why not
 
-# $f4 = PI
 # $f10, $f12, $f14 = arguments 1, 2, 3 (a, b, c)
 # $a0 = argument 4 (level)
 # $f20 = area (saved)
+# $f22, $f24 = ONE, PI
 
 
 	.data	# CONSTANTS
 PI:	.double	3.141592653589793
-SAREA:	.double 2.1547005383792515	# 1 + 2/sqrt(3)
-SRAD:	.double 2.030005838861446	# 3*pi*(1/start_rad)^2
-NEGONE:	.double	-1.0
+SRAD:	.double 2.1547005383792515	# 1 + 2/sqrt(3)
+SAREA:	.double 2.030005838861446	# 3*pi*(1/start_rad)^2
+ONE:	.double	1.0
 
 	.text
 	.globl main
 	
-main:	l.d	$f4, PI		# Load floats
+main:	l.d	$f22, ONE		# Load floats
+	l.d	$f24, PI
 	l.d	$f20, SAREA
 	
-				# Repeat place_circle 3 times
-	l.d	$f10, NEGONE	# Load pcirc arguments -1, SRAD, SRAD, 1
+					# Repeat place_circle 3 times
+	l.d	$f10, ONE		# Load pcirc arguments -1, SRAD, SRAD, 1
+	neg.d	$f10, $f10		# a = -1
 	l.d	$f12, SRAD
 	l.d	$f14, SRAD
 	li	$a0, 1
 	
 	jal	pcirc
 
-	li	$v0, 10		# Exit
+	li	$v0, 10			# Exit
 	syscall
 	
-pcirc:	addi	$sp, $sp, -28	# Push $ra, $a0, $f10, $f12, $f14
-	sw	$ra, 0($sp)	
+pcirc:	addi	$sp, $sp, -28		# Push $ra, $a0, $f10, $f12, $f14
+	sw	$ra, 0($sp)		
 	sw	$a0, 4($sp)
 	s.d	$f10, 8($sp)
 	s.d	$f12, 16($sp)
 	s.d	$f14, 24($sp)
 	
-	jr	$ra		# Return to main
+	mul.d	$f16, $f10, $f12	# d = a*b
+	mul.d	$f18, $f10, $f14	# temp = a*c
+	add.d	$f16, $f16, $f18	# d += temp
+	mul.d	$f18, $f12, $f14	# temp = b*c
+	add.d	$f16, $f16, $f18	# d += temp  (d = a*b + a*c + b*c)
+	sqrt.d	$f16, $f16		# d = sqrt(d)
+	add.d	$f16, $f16, $f16	# d *= 2
+	add.d	$f16, $f16, $f10	# d += a
+	add.d	$f16, $f16, $f12	# d += b
+	add.d	$f16, $f16, $f14	# d += c  (d = 2*sqrt(a*b + a*c + b*c) + a + b + c)
+	
+	div.d	$f16, $f22, $f16	# temp = 1/d
+	mul.d	$f16, $f16, $f16	# temp = temp*temp
+	mul.d	$f16, $f16, $f24	# temp *= PI  
+	add.d	$f20, $f20, $f16	# area += temp  (pi*(1/d)^2)
+
+	
+	jr	$ra			# Return to main
--- src/199.py
+++ src/199.py
@@ -3,12 +3,13 @@ from math import pi
 
 start_rad = 1 + 2/(3**0.5)
 area = 3*pi*(1/start_rad)**2
-MAX_LEVEL = 10
+MAX_LEVEL = 0
 
 def place_circle(a, b, c, level):
     d = 2*(a*b + a*c + b*c)**0.5 + a + b + c
     global area
     area += pi*(1/d)**2
+    print(d, area)
 
     if level < MAX_LEVEL:
         place_circle(a, b, d, level+1)
@@ -16,7 +17,7 @@ def place_circle(a, b, c, level):
         place_circle(b, c, d, level+1)
 
 
-for i in range(3):
+for i in range(3):  # Optimize later
     place_circle(-1, start_rad, start_rad, 1)
 place_circle(start_rad, start_rad, start_rad, 1)
 

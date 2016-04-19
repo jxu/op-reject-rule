699296fa89d9f24c4b38624a2a6859b3227f6e32
jxu <7989982+jxu@users.noreply.github.com>
2016-04-18 20:27:02 -0400

    154 5x speedup with strict combo bounds

--- src/154.cpp
+++ src/154.cpp
@@ -25,15 +25,21 @@ int main()
     int n2 = fact_pow2[200000];
     int n5 = fact_pow5[200000];
 
-    for (int i=0; i<=200000; i++) // Brute force everyday
+    // Generate combos (i, j, k) with i <= j <= k
+    for (int i=0; i<=200000/3; i++)
     {
-        for (int j=i; j<=200000-i; j++)
+        for (int j=i; j<=(200000-i)/2; j++) // i <= j <= 200000-i-j
         {
             int k = 200000-i-j;
-            int d = (i==j) ? 1 : 2; // Count (i,j) and (j,i)
             int ijk2 = fact_pow2[i] + fact_pow2[j] + fact_pow2[k];
             int ijk5 = fact_pow5[i] + fact_pow5[j] + fact_pow5[k];
-            if (n2 - ijk2 >= 12 && n5 - ijk5 >= 12) c+=d;
+            if (n2 - ijk2 >= 12 && n5 - ijk5 >= 12)
+            {
+                // Permutations
+                if (i == j && j == k) c += 1;
+                else if ((i == j && j < k) || (i < j && j == k)) c += 3;
+                else c += 6; // i < j < k
+            }
         }
     }
     std::cout << c << std::endl;

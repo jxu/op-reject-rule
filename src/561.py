189b2b5f5db9c0d333187bc40a632fc2a1981d7e
jxu <7989982+jxu@users.noreply.github.com>
2016-08-01 00:46:48 -0400

    561 more testing

--- src/561.py
+++ src/561.py
@@ -3,8 +3,14 @@
 # Also, (x_1, x_2, ...) != (y_1, y_2, ...) so a and b are distinct
 # S((p_m#)^n) = ((n+1)(n+2)/2)^m - (n+1)^m
 
+# Empirical testing shows:  (f(n) means greatest k such that 2^k divides n)
+# E(odd m, n = 0 mod 4) = f(n//4) + 1
+# E(m, 1 mod 4) = 0
+# E(m, 2 mod 4) = 0
+# E(m, n = 3 mod 4) = m * (f((n+1)//4)+1)  ex. E(10, 15) = 10 * (f(4)+1) = 30
+
 def E_test(m, n):
-    r = pow((n+1)*(n+2)//2, m, 2**100) - pow(n+1, m, 2**100)
+    r = ((n+1)*(n+2)//2)**m - (n+1)**m
     E = 0
     while r%2 == 0:
         r //= 2
@@ -12,6 +18,8 @@ def E_test(m, n):
     print(E, end='\t')
     if n%4 == 3: print()
 
-print('\t', end='')
-for n in range(1, 101):
-    E_test(16, n)
+# Note that Q(8) = 3*m + 3
+def small_m_test():
+    print('\t', end='')
+    for n in range(1, 9):
+        E_test(101, n)

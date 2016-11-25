53d6d6b5968a2e14c39ae1a2f3179a262c015186
jxu <7989982+jxu@users.noreply.github.com>
2016-11-24 22:51:38 -0500

    577

---
+++ src/577.py
@@ -0,0 +1,17 @@
+# We're back, baby!
+# For n, hexagons of size 1: sum from 1 to n-2, inclusive (triangular numbers)
+# For hexagons size x: x-1 inscribed hexagons for a total of x hexagons
+# H(n) = (n-2)(n-1)/2 + 2*(n-5)(n-4)/2 + 3*(n-8)(n-7)/2 + ...
+
+def H(n):
+    result = 0
+    m = n-2
+    inscribed = 1
+    while m >= 1:
+        result += inscribed * m*(m+1)//2
+        inscribed += 1
+        m -= 3
+
+    return result
+
+print(sum(H(n) for n in range(3, 12346)))

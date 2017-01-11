bea4bb08f64c7291980acb8f37da82086e4aaf4a
jxu <7989982+jxu@users.noreply.github.com>
2017-01-11 02:54:03 -0500

    204 failed wheel factorization method

--- src/204.py
+++ src/204.py
@@ -1,2 +1,21 @@
-from number import sieve
-print(sieve(100), len(sieve(100)))
+# Use small wheels similar to sieve of Eratosthenes
+
+from number import sieve, product
+from bisect import bisect_right
+
+def generalised_hamming(cutoff, n):
+    primes = sieve(100)
+    i = bisect_right(primes, cutoff)
+    small_primes = primes[:i]
+    large_primes = primes[i:]
+
+    wheel_size = product(small_primes)
+    nums = [0] * wheel_size
+    for i in small_primes:
+        for j in range(i, wheel_size, i):
+            nums[j] = 1
+
+    not_hamming = [i for i in range(1, wheel_size) if nums[i] == 0]
+    print(wheel_size, len(not_hamming)/float(wheel_size))
+
+generalised_hamming(23, 10**9)

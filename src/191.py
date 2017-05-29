4c7961d45d1578cceb45badb7dc3c57af2d2d805
jxu <7989982+jxu@users.noreply.github.com>
2017-05-29 00:07:06 -0400

    191 full

--- src/191.py
+++ src/191.py
@@ -2,20 +2,22 @@
 
 memo = dict()
 
-def prizes(day, lates, last_absent, s):
+def prizes(day, lates, last_absent):
     if lates < 0 or day < 0: return 0
-    if day == 0:
-        print(s)
-        return 1
+    if day == 0: return 1
 
-    strings =  prizes(day-1, lates, False, 'O'+s)
-    strings += prizes(day-1, lates-1, False, 'L'+s)
-    if not last_absent:
-        strings += prizes(day-1, lates, True, 'A'+s)
-        strings += prizes(day-2, lates, True, 'AA'+s)
+    if (day, lates, last_absent) in memo:
+        return memo[(day, lates, last_absent)]
 
+    strings =  prizes(day-1, lates, False)  # O
+    strings += prizes(day-1, lates-1, False)  # L
+
+    if not last_absent:  # Prevent runs of A longer than 2
+        strings += prizes(day-1, lates, True)  # A
+        strings += prizes(day-2, lates, True)  # AA
+
+    memo[(day, lates, last_absent)] = strings
     return strings
 
 
-print(prizes(4, 1, False, ''))
-
+print(prizes(30, 1, False))

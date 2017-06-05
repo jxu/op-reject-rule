181b9767be8684b22dc882ead0c6b2d28111db5c
jxu <7989982+jxu@users.noreply.github.com>
2017-06-05 02:48:20 -0400

    506 pattern finding

--- src/506.py
+++ src/506.py
@@ -3,12 +3,16 @@ pos = 0
 
 for i in range(1, 50):
     digit_sum = 0
+    digit_seq = 0
     digits = 0
+    old_pos = pos
+
     while digit_sum != i:
         d = seq[pos]
         digit_sum += d
-        digits = 10*digits + d
+        digit_seq = 10*digit_seq + d
 
         pos = (pos+1) % len(seq)
+        digits += 1
 
-    print(digit_sum, digits)
+    print("{:>2}  {}{:>4}  {}".format(digit_sum, old_pos, digits, digit_seq))

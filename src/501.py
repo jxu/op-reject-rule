b9cd30dd8259e506126b424c1fd05b11a974207d
jxu <7989982+jxu@users.noreply.github.com>
2017-06-08 19:26:14 -0400

    501 almost done

---
+++ src/501.py
@@ -0,0 +1,34 @@
+from __future__ import division
+from number import sieve, prime_count
+
+def f(n):
+    primes = sieve(int(n**0.5))
+    count = 0
+
+    # Case 1: n = p^7
+    count += prime_count(int(n**(1/7)))
+
+    # Case 2: n = p^3 * q
+    for p in primes:
+        if p**3 > n: break
+        q_max = n // (p**3)
+        # Exclude case with p == q
+        #print(q_max, prime_count(q_max) - (q_max >= p))
+        count += prime_count(q_max) - (q_max >= p)
+
+    # Case 3: n = p*q*r, p < q < r
+    for p_i in range(len(primes)):
+        p = primes[p_i]
+        if p**3 > n: break
+
+        for q_i in range(p_i + 1, len(primes)):
+            q = primes[q_i]
+            if q > (n / p)**0.5: break  # max q when q = r so n = p * q^2
+            print(p, q)
+            # q < r < n/pq, so add pi(n/pq) - pi(q)
+            count += prime_count(n // (p*q)) - (q_i + 1)
+
+    return count
+
+
+print(f(10**6))

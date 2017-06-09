7998055c1e9cde2544c97e7dce930ef0fbec91dd
jxu <7989982+jxu@users.noreply.github.com>
2017-06-09 00:06:20 -0400

    Add cleaner version of 501

--- src/501.m
+++ src/501.m
@@ -13,3 +13,12 @@ For[pi = 1, pi <= Length[primesCbrt], pi++,
    ];
   ];
 Print[count];
+
+
+(* Cleaner version that won't run in time *)
+n = 10^6;
+primesCbrtN = Prime@Range@PrimePi@CubeRoot[n];
+PrimePi[n^(1/7)] +
+ Sum[PrimePi[n/p^3], {p, primesCbrtN}] - PrimePi[n^(1/4)] +
+ Sum[PrimePi[n/(p q)] - PrimePi[q], {p, primesCbrtN}, {q, 
+   Prime[Range[PrimePi[p] + 1, PrimePi[Sqrt[n/p]]]]}]

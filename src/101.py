9fb96f04242def8df5d0adb110c0542a163e88df
jxu <7989982+jxu@users.noreply.github.com>
2017-06-22 19:54:09 -0400

    101 actually calculated

--- src/101.py
+++ src/101.py
@@ -1,12 +1,29 @@
-from scipy import interpolate
-
 def u(n): return 1 - n + n**2 - n**3 + n**4 - n**5 + n**6 - n**7 + n**8 - n**9 + n**10
 
-FITs = 0
-x = list(range(1, 11))
-y = [u(n) for n in range(1, 11)]
-for d in range(10):
-    z = interpolate.lagrange(x[:d+1], y[:d+1])
-    FITs += round(z(d+2))
+def lazy():
+    from scipy import interpolate
 
-print(FITs)
+    x = list(range(1, 11))
+    y = [u(n) for n in range(1, 11)]
+
+    FITs = 0
+    for d in range(1, 11):
+        z = interpolate.lagrange(x[:d], y[:d])
+        FITs += round(z(d+1))
+
+    return FITs
+
+
+def FIT(k):
+    # Lagrange polynomial interpolation
+    L = 0
+    for j in range(1, k+1):
+        s = u(j)
+        for m in range(1, k+1):
+            if m != j:
+                s *= (k+1 - m) / (j - m)
+
+        L += s
+    return L
+
+print(round(sum(FIT(n) for n in range(1, 11))))

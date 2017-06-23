9df9661eba2c130ad11b047aa37ff1a6883b9380
jxu <7989982+jxu@users.noreply.github.com>
2017-06-22 23:11:40 -0400

    93

---
+++ src/093.py
@@ -0,0 +1,61 @@
+# Kinda brute force, but with RPN at least no need for parentheses
+from __future__ import division
+from itertools import product, permutations
+from number import is_int
+
+def RPN(eq):
+    s = []
+    for t in eq:
+        if type(t) == int:
+            s.append(t)
+        else:
+            y = s.pop()
+            x = s.pop()
+            if t == '+': s.append(x+y)
+            elif t == '-': s.append(x-y)
+            elif t == '*': s.append(x*y)
+            elif t == '/':
+                assert(y != 0)
+                s.append(x/y)
+
+    assert(len(s) == 1)
+    return s[0]
+
+
+def calc(a, b, c, d):
+    op_sets = product(('+', '-', '*', '/'), repeat=3)
+    targets = set()
+    for op_set in op_sets:
+        t = [a, b, c, d] + list(op_set)
+        for eq in permutations(t):
+            # RPN equation must start with 2 numbers and end with operation
+            if type(eq[0]) != int or type(eq[1]) != int \
+                or type(eq[-1]) != str: continue
+
+            try:
+                r = RPN(eq)
+                if r > 0 and is_int(r):
+                    targets.add(round(r))
+            except: pass
+
+    test = 1
+    while test in targets:
+        test += 1
+
+    return test-1
+
+
+max_n = 1
+max_digits = None
+
+for a in range(1, 10):
+    for b in range(a+1, 10):
+        for c in range(b+1, 10):
+            for d in range(c+1, 10):
+                n = calc(a, b, c, d)
+                print(a, b, c, d, n)
+                if n > max_n:
+                    max_n = n
+                    max_digits = 1000*a + 100*b + 10*c + d
+
+print(max_digits)
--- src/number.py
+++ src/number.py
@@ -98,8 +98,17 @@ def is_square_fp(n):
 
 
 def is_square_gmpy(n):
+    # Better to just import gmpy directly?
     import gmpy2
     return gmpy2.is_square(n)
+
+
+def is_close(a, b, tol=1e-9):
+    return abs(a-b) <= tol
+
+
+def is_int(n):
+    return is_close(n, round(n))
 
 
 def prime_factors(n, N_MAX=10000):
@@ -446,6 +455,32 @@ def prime_count(n):
 
     return _pi(n)
 
+
+class unique_element:
+    def __init__(self,value,occurrences):
+        self.value = value
+        self.occurrences = occurrences
+
+
+def unique_permutations(elements):
+    """Like itertools.permutations but without duplicates. Credit: Luka Rahne"""
+    eset=set(elements)
+    listunique = [unique_element(i,elements.count(i)) for i in eset]
+    u=len(elements)
+    return perm_unique_helper(listunique,[0]*u,u-1)
+
+
+def perm_unique_helper(listunique,result_list,d):
+    if d < 0:
+        yield tuple(result_list)
+    else:
+        for i in listunique:
+            if i.occurrences > 0:
+                result_list[d]=i.value
+                i.occurrences-=1
+                for g in  perm_unique_helper(listunique,result_list,d-1):
+                    yield g
+                i.occurrences+=1
 
 
 if __name__ == "__main__":

5ff5ef4808f6c85fbb3b86c993c1d84c98cdc98f
jxu <7989982+jxu@users.noreply.github.com>
2017-07-17 02:45:21 -0400

    118 cleaned up

--- src/118.py
+++ src/118.py
@@ -1,4 +1,4 @@
-# Brute force
+# Brute force, combine together all primes with unique digits
 
 from itertools import permutations
 from number import is_prime
@@ -13,32 +13,24 @@ for l in range(1, 10):
             primes.append(p)
             prime_digits.append(perm)
 
-print(prime_digits[:20])
-print(primes[:100])
-print(len(primes))
-
-
 prime_sets = 0
 
 # pool is an array of bools, with pool[d] representing if d has been used
-def create_set(i, pool, s):
+def create_set(i, pool):
     if all(pool):
-        print(s)
         global prime_sets
         prime_sets += 1
-    #print(i, pool, s)
 
     digits_left = pool.count(False)
-   # print(i, pool, s, digits_left)
 
     for j in range(i+1, len(primes)):
         if len(prime_digits[j]) > digits_left: break
 
         if not any(pool[d] for d in prime_digits[j]):
             for d in prime_digits[j]: pool[d] = True
-            create_set(j, pool, s + [primes[j]])
+            create_set(j, pool)
             for d in prime_digits[j]: pool[d] = False
 
 
-create_set(-1, [True]+[False]*9, [])
+create_set(-1, [True] + [False]*9)
 print(prime_sets)

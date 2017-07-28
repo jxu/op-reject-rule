f0f7c517127b19ab376b29190284f875f2ffe04c
jxu <7989982+jxu@users.noreply.github.com>
2017-07-28 16:05:32 -0400

    128 simplify

--- src/128.py
+++ src/128.py
@@ -1,34 +1,28 @@
 # Brilliant puzzle.
+# Most tiles have two neighbors that have difference 1 and can be ignored.
+# The only tiles that don't are the first and last tile of a ring (ex. 8 and 19)
+# For all tiles (edges AND corners) except for the first and last tiles, the
+# differences between neighbors will be 2 ignored, 2 evens, and 2 odds. Thus
+# tile(x) can't be 3.
 
 from number import sieve
 
-def a(n, s): return 3*n**2 - 3*n + 2 + n*s
+def a(n): return 3*n**2 - 3*n + 2  # Formula for first tile in ring of size n
 
 primes = set(sieve(10**6))
 PD3 = [1]
 
 for n in range(1, 10**5):
-    # First tile: a(n,0)
+    # First tile: a(n)
     if 6*n+1 in primes and 6*n-1 in primes and 12*n+5 in primes:
-        print(a(n, 0))
-        PD3.append(a(n, 0))
+        print(n, a(n))
+        PD3.append(a(n))
 
-    # Corner tiles: a(n,s)
-    for s in range(1, 6):
-        prime_diffs = 0
-        cases = (6*(n-1)+s, 6*n+s, 6*n+s+1, 6*n-s-1)
-        for case in cases:
-            if case in primes: prime_diffs += 1
-
-        if prime_diffs >= 3:
-            print(a(n, s))
-            PD3.append(a(n, s))
-
-    # Last tile: a(n+1,0)-1
+    # Last tile: a(n+1)-1
     if n > 1:
         if 6*n-1 in primes and 12*n-7 in primes and 6*n+5 in primes:
-            print(a(n+1, 0)-1)
-            PD3.append(a(n+1, 0)-1)
+            print(n, a(n+1)-1)
+            PD3.append(a(n+1)-1)
 
 
 print(PD3[2000-1])

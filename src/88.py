a3ef8bfbfc40d2e5d9ae2b41392997c3812f5e6f
jxu <7989982+jxu@users.noreply.github.com>
2018-01-17 11:49:09 -0500

    88 large speedup by enforcing having factor list in ascending order

--- src/88.py
+++ src/88.py
@@ -1,32 +1,30 @@
-# DP saves the day again!
+# Current benchmarks: 13s with python 3, 3s with pypy
+from number import factors
+
+MAX_PSNUM = 13000
+sorted_divisors = [factors(n) for n in range(MAX_PSNUM)]
+
 # Uses function that keeps track of sum, product, and sum/product list size
-
-from number import memoize, factors
-
-MAX_PSNUM = 15000
-sorted_divisors = [sorted(factors(n)) for n in range(MAX_PSNUM)]
-
-
-@memoize
-def f(s, p, k):
+#actually faster without memoization (!)
+def f(s, p, k, min_d):
     if s < 0: return False
-    if k == 0:
-        return s == 0 and p == 0
-    if s == p and k == 1: return True  # Fill remaining list with 1s
-    if p == 1:
-        return s == k
+    if k == 0: return s == 0 and p == 0
+    if k == 1: return s == p
+    if p == 1: return s == k  # Fill remaining list with 1s
 
     #print(s,p,k)
     for d in sorted_divisors[p]:
-        if d == 1: continue  # Avoid filling list with 1s
-        if f(s-d, p//d, k-1): return True
+        # Enforce using divisors in ascending order (5x speedup)
+        if d < min_d: continue
+        if f(s-d, p//d, k-1, d): return True
     return False
 
 min_set = set()
 
 for k in range(2, 12001):
     for s in range(k+1, MAX_PSNUM):  # sum > k
-        if f(s, s, k):
+        # Doesn't save time to skip primes
+        if f(s, s, k, 2):  # Min divisor of 2 to avoid filling list with 1s
             print(k, s)
             min_set.add(s)
             break

3abd7ef64d8f9fead5f84ff4c4b7197426dcab73
jxu <7989982+jxu@users.noreply.github.com>
2018-01-27 22:00:07 -0500

    106 almost closed form

--- src/106.py
+++ src/106.py
@@ -2,6 +2,7 @@
 # can be paired 1-to-1 with an element b in s2 such that a > b
 # Ex. For a<b<c<d, a<c and b<d => a+c<b+d
 # Brute force all subset pairs of equal size and try to pair using backtrack
+# (See below for better solution)
 
 from itertools import combinations
 
@@ -36,9 +37,29 @@ def count_test(n):
                     print(s1, s2)
                     certain_pairs += 1
 
-
         s += subset_pairs//2 - certain_pairs
 
     return s
 
-print(count_test(12))
+# Cleaner method: Note that subset pair can be represented as ternary string,
+# with ith char 0 = not in either set, 1 = in s1, 2 = s2
+# Ex. 121020 represents (0, 2) (1, 4)
+# Experimentally Catalan number of pairs don't need to be tested:
+# as long as the string is a Dyck word, i.e. no prefix has more 2s than 1s
+
+# The number of strings guaranteeing inequality is C_k * (n choose 2k)
+# The total number of subset pairs is (n choose k)*(n-k choose k)/2
+# Expression can be simplified further
+from number import combination
+
+def count_test2(n):
+    s = 0
+    for k in range(2, n//2 + 1):
+        certain_strings = combination(n, 2*k) * combination(2*k, k) // (k+1)
+        subset_pairs = combination(n, k) * combination(n-k, k) // 2
+        s += subset_pairs - certain_strings
+
+    return s
+
+#print(count_test(12))
+print(count_test2(12))

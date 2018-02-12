63c5dd9ce005604272c6275cede2f172503e4dd5
jxu <7989982+jxu@users.noreply.github.com>
2018-02-12 18:33:38 -0500

    152 use fixed point arithmetic

--- src/152.py
+++ src/152.py
@@ -1,36 +1,44 @@
 from __future__ import division
-from number import memoize
-from fractions import Fraction
+from number import memoize, lcm
+#from fractions import Fraction
 
 x = 0
 
-D_MAX = 80
-tail_sum = [0]*(D_MAX+1)
-tail_sum[D_MAX] = 1/(D_MAX**2)
-for i in range(D_MAX, 1, -1):
-    tail_sum[i] = tail_sum[i+1] + 1/(i**2)
+D_MAX = 45
+lcm_denom = 1
+for i in range(2, D_MAX+1):
+    lcm_denom = lcm(lcm_denom, i**2)
+
+print(lcm_denom)
+
+numers = [0]*(D_MAX+1)
+for i in range(2, D_MAX+1):
+    numers[i] = lcm_denom // (i**2)
+
+tail_sums = [0]*(D_MAX+1)
+tail_sums[D_MAX] = numers[D_MAX]
+for i in range(D_MAX-1, 1, -1):
+    tail_sums[i] = tail_sums[i+1] + numers[i]
 
 
 @memoize
 def f(target, lo, hi):
     if lo > hi or target < 0: return 0
-    if float(target) - 1/(hi**2) < 0: return 0
-    if tail_sum[lo] < target: return 0
+    if numers[hi] > target: return 0
+    if tail_sums[lo] < target: return 0
 
     if target == 0:
         return 1
+
 
     global x
     x += 1
 
     s = 0
     for denom in range(lo, hi+1):
-        s += f(target - Fraction(1, denom**2), denom+1, hi)
+        s += f(target - numers[denom], denom+1, hi)
 
     return s
 
-print(f(Fraction(1, 2), 2, 20))
+print(f(lcm_denom//2, 2, 20))
 print(x)
-
-
-

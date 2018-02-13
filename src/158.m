7ca7e99bf3afd3ae68a0b94d7de3fc0610c4f578
jxu <7989982+jxu@users.noreply.github.com>
2018-02-12 21:35:48 -0500

    158

---
+++ src/158.m
@@ -0,0 +1,11 @@
+(* A string with one spot with characters in ascending order is the same as 
+  two strings of descending order separated by one ascention.
+  Write the first descending string as xxx...x and the second as yyy...y
+  There are 2^n strings made up of x and y. Exclude 2 that are only x and only
+  y. 
+  There are n-1 forbidden strings that have no ascention: xy...y, xxy...y,
+  xxxy...y, ..., xxx...xy. 
+  So the closed form solution is (26 choose n) * (2^n - 2 - (n-1))  *)
+
+Max[Table[Binomial[26, n]*(2^n - n - 1), {n, 1, 26}]]
+   
--- src/158.py
+++
@@ -1,2 +0,0 @@
-ltl_count = [[0]*26 for _ in range(25)]  # Ways to get from one letter to another
-ltl_count[0] = [1]*26

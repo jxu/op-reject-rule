60834653513a6fb1d8e43c1c402774fc577c5f4a
jxu <7989982+jxu@users.noreply.github.com>
2018-02-14 16:53:24 -0500

    618 rearrange things

--- src/618.py
+++ src/618.py
@@ -1,29 +1,33 @@
-
+from __future__ import division
 from number import fib_list, sieve, memoize
-#import functools
-import sys
 
-#sys.setrecursionlimit(5000)
 
 fib = fib_list(24)
 primes = sieve(fib[-1])
 
 @memoize
-def S(k, min_p=2):
+def S(k, p_i=0):
     if k < 0: return 0
     if k == 0: return 1
 
+    p = primes[p_i]
+    if k == p: return p
+    if p > k//2: return 0
+
     s = 0
-    for p in primes:
-        if p < min_p: continue
-        if p > k: break
-        s = (s + p * S(k-p, p)) % 10**9
+
+    pow_p = 1
+    for j in range(k//p + 1):
+        s = (s + pow_p * S(k - j*p, p_i+1)) % 10**9
+        pow_p = (pow_p * p) % 10**9
 
     return s
 
+
 print(S(8))
+
 # Prevent exceeding recursion limit by pre-calculating smaller S(k)
-for k in range(100, fib[-1], 100):
+for k in range(1000, fib[-1], 1000):
     print(k, S(k), len(S.cache))
 
 print(sum(S(f) for f in fib[2:]) % 10**9)

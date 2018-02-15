15dccf75f0b0c6163df3ba6ef6dec651aadaec30
jxu <7989982+jxu@users.noreply.github.com>
2018-02-15 00:46:54 -0500

    618 skip large primes approach

--- src/618.py
+++ src/618.py
@@ -4,6 +4,7 @@ from number import fib_list, sieve, memoize
 
 fib = fib_list(24)
 primes = sieve(fib[-1])
+sp = set(primes)
 
 @memoize
 def S(k, p_i=0):
@@ -11,8 +12,11 @@ def S(k, p_i=0):
     if k == 0: return 1
 
     p = primes[p_i]
-    if k == p: return p
-    if p > k//2: return 0
+    if p > k: return 0
+    if k not in sp:
+        if 2*p > k: return 0
+        if 3*p > k and k-p not in sp:
+            return S(k, p_i+1) % 10**9
 
     s = 0
 
@@ -23,11 +27,13 @@ def S(k, p_i=0):
 
     return s
 
-
-print(S(8))
+print(S(1), S(2), S(3), S(5), S(8))
+assert (S(1), S(2), S(3), S(5), S(8)) == (0, 2, 3, 11, 49)
 
 # Prevent exceeding recursion limit by pre-calculating smaller S(k)
 for k in range(1000, fib[-1], 1000):
     print(k, S(k), len(S.cache))
+    if k == 1000: assert(S(k) == 542144285)
+
 
 print(sum(S(f) for f in fib[2:]) % 10**9)

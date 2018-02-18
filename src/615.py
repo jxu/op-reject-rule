7483e52fea587e14061f5a74c0e7d39f2aecebdb
jxu <7989982+jxu@users.noreply.github.com>
2018-02-18 00:29:34 -0500

    615 more tables

--- src/615.py
+++ src/615.py
@@ -1,12 +1,16 @@
 # Let M = 10^6. Smallest num with 1 million prime factors: 2^M
-# Upper bound: 2^(M-1) * p_M, with p_M about 1.55e7
+# Upper bounds: 2^(M-1) * M,
+# 2^(M-1)*n with n having at least 1 prime factor,
+# 2^(M-2)*n with n having at least 2 prime factors,
+# ... for 2^(M-4)*n with n having 4 or more prime factors. There are ~M such
+# n below 2.11e6.
 from __future__ import division
 from number import sieve
 import heapq
 
 # Parameters to tune
-DEPTH = 9
-max_num = 10**11
+DEPTH = 22
+max_num = 10**12
 M = 10**6
 PRIMES = sieve(10**7)
 MAX_I = len(PRIMES)
@@ -17,10 +21,10 @@ def build(product, depth, min_i):
 
     if depth >= DEPTH:
         heapq.heappush(h, product)
-        if len(h) >= M and len(h) % 10**6 == 0:
+        if len(h) >= M and len(h) % 10**4 == 0:
             global max_num
             max_num = min(max_num, h[M-1])
-            print(len(h), max_num)
+            print(len(h), max_num, max_num/(2**DEPTH))
 
     for i in range(min_i, MAX_I):
         if product * PRIMES[i] >= max_num: break

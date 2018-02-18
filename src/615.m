6d209985db9259f8c8063356464f410d890145c1
jxu <7989982+jxu@users.noreply.github.com>
2018-02-17 23:35:57 -0500

    615 tables and experimentation

---
+++ src/615.m
@@ -0,0 +1,3 @@
+Select[Range[10^6, 1.1*10^6], # - PrimePi[#] >= 10^6 &, 1]
+Select[Range[2*10^6], PrimeOmega[#] > 2 &][[10^6]]
+Select[Range[2.5*10^6], PrimeOmega[#] > 3 &][[10^6]]
--- src/615.py
+++ src/615.py
@@ -2,29 +2,33 @@
 # Upper bound: 2^(M-1) * p_M, with p_M about 1.55e7
 from __future__ import division
 from number import sieve
+import heapq
 
 # Parameters to tune
-DEPTH = 10
-MAX_NUM = 10**10
+DEPTH = 9
+max_num = 10**11
 M = 10**6
-PRIMES = sieve(10**6)
+PRIMES = sieve(10**7)
 MAX_I = len(PRIMES)
 
-l = []
+h = []
 
 def build(product, depth, min_i):
 
     if depth >= DEPTH:
-        l.append(product)
+        heapq.heappush(h, product)
+        if len(h) >= M and len(h) % 10**6 == 0:
+            global max_num
+            max_num = min(max_num, h[M-1])
+            print(len(h), max_num)
 
     for i in range(min_i, MAX_I):
-        if product * PRIMES[i] >= MAX_NUM: break
+        if product * PRIMES[i] >= max_num: break
         build(product*PRIMES[i], depth+1, i)
 
 
 build(1, 0, 0)
-print(len(l))
-n = sorted(l)[M-1]
+n = h[M-1]
 
-print(n, n/(2**DEPTH))
+print(n, n/(2**DEPTH), len(h))
 print((pow(2, M - DEPTH, 123454321) * n) % 123454321)

0303e4a4851148b7c56c92f50135c5ae3cc5f3a9
jxu <7989982+jxu@users.noreply.github.com>
2018-05-08 00:32:06 -0400

    618 use large memo table

--- src/618.py
+++ src/618.py
@@ -1,39 +1,56 @@
+# Using memoize decorator was slower than allocating a large lookup table
+
 from __future__ import division
 from number import fib_list, sieve, memoize
+import sys
+
+sys.setrecursionlimit(50000)
 
 
 fib = fib_list(24)
 primes = sieve(fib[-1])
 sp = set(primes)
 
-@memoize
+memo_maxk = fib[-1]
+memo_maxi = len(primes)
+memo = [[-1]*memo_maxi for _ in range(memo_maxk)]
+
+#@memoize
 def S(k, p_i=0):
-    if k < 0: return 0
+    if k < 0 or p_i >= len(primes): return 0
     if k == 0: return 1
 
+    if memo[k][p_i] != -1:
+        return memo[k][p_i]
+
     p = primes[p_i]
-    if p > k: return 0
-    if k not in sp:
-        if 2*p > k: return 0
-        if 3*p > k and k-p not in sp:
-            return S(k, p_i+1) % 10**9
-
     s = 0
+    if p > k: s = 0
+    elif k not in sp and 2*p > k: s = 0
+        #elif 3*p > k and k-p not in sp:
+        #    s = S(k, p_i+1) % 10**9
 
-    pow_p = 1
-    for j in range(k//p + 1):
-        s = (s + pow_p * S(k - j*p, p_i+1)) % 10**9
-        pow_p = (pow_p * p) % 10**9
+    else:
+        pow_p = 1
+        for j in range(k//p + 1):
+            s = (s + pow_p * S(k - j*p, p_i+1)) % 10**9
+            pow_p = (pow_p * p) % 10**9
 
+    memo[k][p_i] = s
     return s
 
-print(S(1), S(2), S(3), S(5), S(8))
+# Sanity test
 assert (S(1), S(2), S(3), S(5), S(8)) == (0, 2, 3, 11, 49)
 
-# Prevent exceeding recursion limit by pre-calculating smaller S(k)
-for k in range(1000, fib[-1], 1000):
-    print(k, S(k), len(S.cache))
-    if k == 1000: assert(S(k) == 542144285)
+# Precompute some values of S(k)
+for k in range(0, fib[-1], 100):
+    print(k, S(k))
 
+final_sum = 0
+for f in fib[2:]:
+    s = S(f)
+    print(f, s)
+    final_sum += s
 
-print(sum(S(f) for f in fib[2:]) % 10**9)
+print(final_sum % 10**9)
+
--- src/622.m
+++ src/622.m
@@ -1,5 +1,3 @@
-(* ::Package:: *)
-
 (* Testing riffle shuffle *)
 shuffle[d_] := Riffle @@ Partition[d, Length[d]/2]; 
 NestList[shuffle, Range[0, 9], 6]
@@ -10,6 +8,3 @@ NestList[shuffle, Range[0, 9], 6]
 (* 2^60 = 1 (mod 2n+1), so consider factors of 2^60 - 1 *)
 First@Total@Select[Table[{n+1,MultiplicativeOrder[2,n]},{n,Divisors[2^60-1]}],Last[#]==60&]
 Total[1+Select[Divisors[2^60 - 1], MultiplicativeOrder[2, #] == 60 &]  (* Shorter solution by Zach-Lurf *)
-
-
-

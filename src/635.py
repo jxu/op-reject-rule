4c68fa2b8a1e6273ff13819f79284872db9fd250
jxu <7989982+jxu@users.noreply.github.com>
2018-09-04 15:26:29 -0400

    635 clean up slightly

--- src/635.py
+++ src/635.py
@@ -1,24 +1,20 @@
 # IMO 1995 Problem 6 (thanks to my friend John for pointing this out)
 # Generating function approach with roots-of-unity filter described in
 # http://zacharyabel.com/papers/Multi-GF_A06_MathRefl.pdf
-# Timing (ThinkPad) Python 3.6: 10m 25s, PyPy 5.10.0: 1m37
+# Timing (ThinkPad) Python 3.6: 10m 25s, PyPy 5.10.0: 1m 37s
 
 from number import sieve, mul_inv
 M = 1000000009
 L = 10**8
-primes = sieve(L)
-sp = set(primes)
+sp = set(sieve(L))
 
 A2 = -2  # adjust for A2(2) = 2 instead of 4
 A3 = -3  # A3(2) = 6 instead of 9
 bin2 = 1 # (2n choose n)
 bin3 = 1 # (3n choose n)
 for n in range(L - 1):
-
     if n in sp:
         p = n
-        if p % 10**5 == 1:
-            print(p, bin2, bin3)
         p_inv = mul_inv(p, M)
         A2 = (A2 + p_inv * (bin2 + 2*(p-1))) % M
         A3 = (A3 + p_inv * (bin3 + 3*(p-1))) % M

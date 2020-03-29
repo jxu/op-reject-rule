d016cc772a651994a2caa33452803aa495539e5d
jxu <7989982+jxu@users.noreply.github.com>
2020-03-29 01:52:44 -0400

    618

--- src/618.py
+++ src/618.py
@@ -1,56 +1,22 @@
-# Using memoize decorator was slower than allocating a large lookup table
+# Simple DP (F_24 ~ 50000, about 5000 primes below F_24)
+# Recursion runs out of stack memory using Python 3.6.
+# Runs fine in about 1.5 mins with Pypy 2.7 and 3.6
 
-from __future__ import division
+# (also A002098) G.f. approach is
+# Product_p ((p x^p)^0 + (p x^p)^1 + (p x^p)^2 + ...)
+# = Product_p 1 / (1 - p x^p)
+
 from number import fib_list, sieve, memoize
 import sys
-
-sys.setrecursionlimit(50000)
-
+sys.setrecursionlimit(30000)
 
 fib = fib_list(24)
 primes = sieve(fib[-1])
-sp = set(primes)
 
-memo_maxk = fib[-1]
-memo_maxi = len(primes)
-memo = [[-1]*memo_maxi for _ in range(memo_maxk)]
-
-#@memoize
-def S(k, p_i=0):
-    if k < 0 or p_i >= len(primes): return 0
+@memoize
+def S(k, i=len(primes)-1):
+    if k < 0 or i < 0: return 0
     if k == 0: return 1
+    return (S(k, i-1) + primes[i] * S(k - primes[i], i)) % 10**9
 
-    if memo[k][p_i] != -1:
-        return memo[k][p_i]
-
-    p = primes[p_i]
-    s = 0
-    if p > k: s = 0
-    elif k not in sp and 2*p > k: s = 0
-        #elif 3*p > k and k-p not in sp:
-        #    s = S(k, p_i+1) % 10**9
-
-    else:
-        pow_p = 1
-        for j in range(k//p + 1):
-            s = (s + pow_p * S(k - j*p, p_i+1)) % 10**9
-            pow_p = (pow_p * p) % 10**9
-
-    memo[k][p_i] = s
-    return s
-
-# Sanity test
-assert (S(1), S(2), S(3), S(5), S(8)) == (0, 2, 3, 11, 49)
-
-# Precompute some values of S(k)
-for k in range(0, fib[-1], 100):
-    print(k, S(k))
-
-final_sum = 0
-for f in fib[2:]:
-    s = S(f)
-    print(f, s)
-    final_sum += s
-
-print(final_sum % 10**9)
-
+print(sum(S(fib[i]) for i in range(2, 25)) % 10**9)

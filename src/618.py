57e4ea796d7cfeeabb2b93ccaa8bbf35f79e2fcf
jxu <7989982+jxu@users.noreply.github.com>
2020-03-29 02:24:18 -0400

    618 iterative version

--- src/618.py
+++ src/618.py
@@ -19,4 +19,16 @@ def S(k, i=len(primes)-1):
     if k == 0: return 1
     return (S(k, i-1) + primes[i] * S(k - primes[i], i)) % 10**9
 
-print(sum(S(fib[i]) for i in range(2, 25)) % 10**9)
+#print(sum(S(fib[i]) for i in range(2, 25)) % 10**9)
+
+# Much faster (0.5 s) iterative version that overwrites DP array
+# based on Robert_OConnor's solution
+def S2(k):
+    m = [1] + [0] * fib[-1]
+    for p in primes:
+        for j in range(p, k+1):
+            m[j] = (m[j] + p * m[j-p]) % 10**9
+    return m
+
+m = S2(fib[-1])
+print(sum(m[fib[i]] for i in range(2, 25)) % 10**9)

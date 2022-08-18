38c682ce10ab4c59a53b6ca67f6312f3b096345b
jxu <7989982+jxu@users.noreply.github.com>
2022-08-18 04:13:17 -0400

    804 try using Decimal for rounding

--- src/804.py
+++ src/804.py
@@ -1,16 +1,26 @@
-def T(N):
+from decimal import Decimal, getcontext, ROUND_DOWN
 
-    y_max = int((4*N / 163)**0.5)
+getcontext().prec = 50
+
+def round_down(x):  # round to zero
+    return x.to_integral_value(rounding=ROUND_DOWN)
+
+def T(N):
+    N = Decimal(N)
+    y_max = round_down((4*N / 163).sqrt())
     s = -1  # exclude (0,0)
 
-    for y in range(-y_max, y_max+1):
+    y = -y_max
+    while y <= y_max:
         disc = 4*N - 163*y**2
-        err = (disc**0.5)**2 - disc
+        sqrt_disc = disc.sqrt()
+        #err = sqrt_disc**2 - disc
         #print(err)
-        x_min = int((- disc**0.5 - y) / 2)
-        x_max = int((disc**0.5 - y) / 2)
+        x_min = round_down((- sqrt_disc - y) / 2)
+        x_max = round_down((sqrt_disc - y) / 2)
         #print(y, x_min, x_max)
         s += x_max - x_min + 1
+        y += 1
 
     return s
 

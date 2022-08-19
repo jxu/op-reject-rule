6dbec22472ba190b346b8eb4439f59d04c9e3f6b
jxu <7989982+jxu@users.noreply.github.com>
2022-08-19 02:05:34 -0400

    804 fix with explanation

--- src/804.py
+++ src/804.py
@@ -1,26 +1,34 @@
-from decimal import Decimal, getcontext, ROUND_DOWN
+# Although g(n) is formulated for exact n, the problem is just counting
+# lattice points within the (slightly rotated) ellipse
+# (No quadratic number fields here)
+# 1 <= x^2 + xy + 41y^2 <= N
 
-getcontext().prec = 50
+# (Counting x values per y is more efficient, since fewer y's)
+# lower bound only excludes (0,0)
+# x values given by (-sqrt(D)-y)/2 <= x <= (sqrt(D)-y)/2 in constant time
+# where disc D = 4N - 163y^2, and y range is when disc >= 0 (O(sqrt N) y's)
 
-def round_down(x):  # round to zero
-    return x.to_integral_value(rounding=ROUND_DOWN)
+# Aside: My original wrong answer was 4921370551114716. I thought it was an
+# error due to doubles only having ~15 digits of precision, but turns out it
+# was me doing rounding wrong. I assumed x_min < 0 and x_max > 0 so used
+# int(x) which rounds towards zero, instead of floor and ceil.
+
+# Other ideas from problem thread:
+# endagorion: maintain [x_min, x_max] and change interval through changing y
+# ironman353 / ecnerwala: actual quadratic number field solution
+# griff / abcwuhang: convex hull trick in O(N^1/3)
+
+from math import floor, ceil
 
 def T(N):
-    N = Decimal(N)
-    y_max = round_down((4*N / 163).sqrt())
+    y_max = int((4*N / 163)**0.5)
     s = -1  # exclude (0,0)
 
-    y = -y_max
-    while y <= y_max:
-        disc = 4*N - 163*y**2
-        sqrt_disc = disc.sqrt()
-        #err = sqrt_disc**2 - disc
-        #print(err)
-        x_min = round_down((- sqrt_disc - y) / 2)
-        x_max = round_down((sqrt_disc - y) / 2)
-        #print(y, x_min, x_max)
+    for y in range(-y_max, y_max+1):
+        sqrt_disc = (4*N - 163*y**2)**0.5
+        x_min = ceil((- sqrt_disc - y) / 2)
+        x_max = floor((sqrt_disc - y) / 2)
         s += x_max - x_min + 1
-        y += 1
 
     return s
 

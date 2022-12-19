8b7b7aee01849fc321eb8ce80b3b5cf2df41042f
jxu <7989982+jxu@users.noreply.github.com>
2022-12-18 19:20:11 -0500

    816 refactor to use complex points and cleanup

--- src/816.py
+++ src/816.py
@@ -1,50 +1,57 @@
-def dist(p0, p1):
-    return ((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2) ** 0.5
+# Nearest pair of points divide-and-conquer algorithm by Shamos
+# Section 5.4 of Algorithm Design book
 
-# split points in linear time
+# Silly idea: store 2D points as complex numbers
+# Pypy3 Thinkpad benchmark- points as tuples: 27s, points as complex: 16s
+
+def dist(p0, p1):
+    return abs(p0 - p1)
+
+# partition points into left and right parts in linear time
 def left_half(Px, Py, L):
-    return ([p for p in Px if p[0] < L],
-            [p for p in Py if p[0] < L])
+    return ([p for p in Px if p.real < L],
+            [p for p in Py if p.real < L])
 
 def right_half(Px, Py, L):
-    return ([p for p in Px if p[0] >= L],
-            [p for p in Py if p[0] >= L])
+    return ([p for p in Px if p.real >= L],
+            [p for p in Py if p.real >= L])
 
 
-# requires sorted points sorted by x
-# Px, Py: points sorted by x and y
+# Main divide-and-conquer algorithm
+# requires Px, Py: points sorted by x and y
 def closest_pair(Px, Py):
     n = len(Px)
     if n < 2: return float("inf")
     if n == 2: return dist(Px[0], Px[1])
 
-    L =  Px[n//2][0]  # midway line
+    L =  Px[n//2].real  # midway line
 
     D = min(closest_pair(*left_half(Px, Py, L)),
             closest_pair(*right_half(Px, Py, L)))
-    #print(L)
-    Sy = [p for p in Py if abs(p[0] - L) < D]
 
+    Sy = [p for p in Py if abs(p.real - L) < D]
+
+    # magic strip
     for i in range(len(Sy)):
-        for j in range(1, 16):
-            if i+j < len(Sy):
-                D = min(dist(Sy[i], Sy[i+j]), D)
+        for j in range(1, min(16, len(Sy)-i)):
+            D = min(dist(Sy[i], Sy[i+j]), D)
 
     return D
+
 def d(k):
     # RNG ensures no pair of points have same x or y coord
+    # (unless points repeat)
     s = [290797]
     P = []
     for n in range(2*k):
         s.append(s[-1]**2 % 50515093)
         if n % 2 == 0:
-            P.append(s[-2:])
+            P.append(s[-2] + s[-1]*1j)
 
-    print(P[:10])
 
-    Px = sorted(P, key=lambda p:p[0])
-    Py = sorted(P, key=lambda p:p[1])
+    Px = sorted(P, key=lambda p:p.real)
+    Py = sorted(P, key=lambda p:p.imag)
 
     return closest_pair(Px, Py)
 
-print(round(d(2*10**6),9))
+print(round(d(2000000),9))

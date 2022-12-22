b38312d9403c40b3972a4ee568845092512bbab7
jxu <7989982+jxu@users.noreply.github.com>
2022-12-22 12:17:08 -0500

    820 add explanation and one-liner

--- src/820.py
+++ src/820.py
@@ -1,6 +1,5 @@
-# Determine nth decimal digit directly: from long division,
-# by subtracting out multiples of k, nth (0-index) remainder is
-# 10^n mod k
+# Determine nth decimal digit directly: from long division, by subtracting out
+# multiples of k, nth (0-index) remainder is 10^n mod k
 
 def d(n, k):
     rem = pow(10, n-1, k)
@@ -11,3 +10,6 @@ def S(n):
     return sum(d(n,k) for k in range(1,n+1))
 
 print(S(10**7))
+
+# one-liner
+n = 10**7; print(sum((10 * pow(10, n-1, k)) // k for k in range(1,n+1)))

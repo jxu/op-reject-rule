475b9bf9618aec9141ffa8d9de5c45d840207e60
jxu <7989982+jxu@users.noreply.github.com>
2022-12-21 21:40:57 -0500

    820 small scale solution

--- src/820.py
+++ src/820.py
@@ -2,35 +2,38 @@
 
 def d(n, k):
     rems = []
+    rems_set = set()
     qs = []
     rem = 1
     rep_i = 0
-    for i in range(10):
+    while True:
         d = rem * 10
         q = d // k
         #print(d, q, rem, rems, qs)
 
-        try:
+        if rem in rems_set:
             rep_i = rems.index(rem)
             #print(rep_i, rem, rems, qs)
             break
-        except: pass
 
         rems.append(rem)
+        rems_set.add(rem)
         qs.append(q)
         rem = d - k * q
 
-    print(rep_i, rems, qs)
+    #print(rep_i, rems, qs)
     dec_start = qs[:rep_i]
     repetend = qs[rep_i:]
 
-    print(dec_start, repetend)
+    #print(dec_start, repetend)
+    if k % 1000 == 1: print(k, len(repetend))
+    # len(repetend) = A007732
     idx = (n - 1 - len(dec_start)) % len(repetend)
     return repetend[idx]
 
-print(d(4,4))
+#print(d(4,4))
 
 def S(n):
     return sum(d(n,k) for k in range(1,n+1))
 
-#print(S(100))
+print(S(10**5))

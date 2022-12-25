26786c5680f47719ce82b9014aa1df002eef4a13
jxu <7989982+jxu@users.noreply.github.com>
2022-12-25 10:43:15 -0500

    81 refactor a little

--- src/081.py
+++ src/081.py
@@ -1,14 +1,17 @@
+# Classic DP problem
+import csv
+from copy import deepcopy
+N = 80
 with open("p081_matrix.txt", 'r') as f:
-    matrix = f.read().splitlines()
-    matrix = [list(map(int, row.split(','))) for row in matrix]
+    matrix = [list(map(int, row)) for row in csv.reader(f)]
 
-path = matrix[:]
+path_sum = deepcopy(matrix)
+for i in range(N):
+    for j in range(N):
+        if (i, j) == (0, 0): continue
+        elif i == 0:         path_sum[i][j] += path_sum[i][j-1]
+        elif j == 0:         path_sum[i][j] += path_sum[i-1][j]
+        else:                path_sum[i][j] += min(path_sum[i][j-1],
+                                                   path_sum[i-1][j])
 
-for i in range(80):
-    for j in range(80):
-        if (i, j) == (0, 0):    continue
-        if i == 0:              path[i][j] += path[i][j-1]
-        elif j == 0:            path[i][j] += path[i-1][j]
-        else:                   path[i][j] += min(path[i][j-1], path[i-1][j])
-
-print(path[79][79])
+print(path_sum[N-1][N-1])

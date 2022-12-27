3fb3a502b7491160958113a3e2fcc9e1bf996d0d
jxu <7989982+jxu@users.noreply.github.com>
2022-12-26 20:54:44 -0500

    518 progress

--- src/518.py
+++ src/518.py
@@ -1,21 +1,25 @@
 # b'^2 = a' * c'
-import number,time
+import number
 def S(n):
+    s = 0
     primes = number.sieve(n)
     sp = set(primes)
-    print(primes[:30])
 
-    #factored = [number.prime_factors(p+1) for p in primes]
-    #print(factored[:10])
-    for p in primes[1:]:
-        b = [2] + number.prime_factors((p+1)//2)
-        if p % 1000 == 1:
-            print(p, b)
+    for b in primes:
+        b1 = b + 1
+        for a in primes:
+            if a >= b: break
 
+            a1 = a + 1
+            if a1 < b1**2 / n: continue
 
+            if b1**2 % a1 == 0:
+                c1 = (b1**2 // a1)
+                c = c1 - 1
+                if c in sp:
+                    print(a, b, c)
+                    s += a + b + c
 
+    return s
 
-
-start = time.process_time()
-S(10**6)
-print(time.process_time()-start)
+print(S(10**8))

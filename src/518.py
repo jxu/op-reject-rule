101882793f315bea2e95ed219fdaf564233d0ebb
jxu <7989982+jxu@users.noreply.github.com>
2022-12-27 18:51:33 -0500

    518 try exiting a1 early

--- src/518.py
+++ src/518.py
@@ -9,17 +9,18 @@ def S(n):
     for b in primes:
         b1 = b + 1
         pps = factor(b1**2, primes)
-        divs = divisors(pps)
-        for a1 in divs:
+        divs = sorted(divisors(pps))
+        for a1 in reversed(divs):
             c1 = b1**2 // a1
             a = a1 - 1
             c = c1 - 1
+            if c >= n: break
             if c1 > b1 and a in sp and c in sp:
                 print(a, b, c)
                 s += a + b + c
 
     return s
 
-print(S(10**8))
+print(S(10**6))
 
 

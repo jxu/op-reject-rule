3e694701b2a6bf0ecbc1c1ebd6145ac4cb231a02
jxu <7989982+jxu@users.noreply.github.com>
2022-12-28 17:11:55 -0500

    417 improve algorithm

--- src/417.py
+++ src/417.py
@@ -1,4 +1,10 @@
-# 10 hours of brute force
+# By long division, period of 1/n is just mult order of 10 mod n
+# Factors of 2 and 5 can be divided out without affecting period
+# For a,b coprime, order 10 mod ab = lcm(order 10 mod a, order 10 mod b)
+# by CRT. So we can factor n into prime powers and take the LCM of orders
+
+# Benchmark: P51 with sympy n_order and functools memoize, factoring takes 14m
+# Without factoring takes 25m
 from number import factor, sieve, lcm, memoize
 from sympy.ntheory import n_order
 
@@ -9,19 +15,15 @@ def ord10(n):
     return n_order(10, n)
 
 def L(n):
+    if n % 10000 == 0: print(n)
     while n % 2 == 0: n //= 2
     while n % 5 == 0: n //= 5
     if n == 1: return 0
 
     o = 1
     for p, e in factor(n, primes).items():
+        o = lcm(o, ord10(p**e))
 
+    return o
 
-    return ord10(n)
-
-s = 0
-for i in range(1, 10**1):
-    print(i)
-    s += L(i)
-
-print(s)
+print(sum(L(i) for i in range(1, 10**8)))

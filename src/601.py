f6edf64322aa3cc9d0bc120edea1ba6f6b2ac818
jxu <7989982+jxu@users.noreply.github.com>
2022-12-28 11:35:16 -0500

    601 reduce code

--- src/601.py
+++ src/601.py
@@ -1,20 +1,12 @@
 # "Streaking" to victory :3
 # n = 1 mod 2, 1 mod 3, ..., 1 mod k, n != 1 mod k+1
-# math.lcm is python 3.9+
-from number import lcm
+from number import lcm  # math.lcm is python 3.9+
 from functools import reduce
 
 def lcm_n(n):
     return reduce(lcm, range(1, n+1), 1)
 
 def P(s, N):
-    count = 0
-    for n in range(1, N, lcm_n(s)):
-        if n % (s+1) != 1: count += 1
-    return count
+    return sum(n % (s+1) != 1 for n in range(1, N, lcm_n(s)))
 
-
-s = 0
-for i in range(1, 32):
-    s += P(i, 4**i)
-print(s)
+print(sum(P(i, 4**i) for i in range(1, 32)))

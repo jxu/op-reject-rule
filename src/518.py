8fdeacd90fd6fdcfdc3756127c3902e569fde5aa
jxu <7989982+jxu@users.noreply.github.com>
2022-12-29 15:58:13 -0500

    518 speedup using dev_random method

--- src/518.py
+++ src/518.py
@@ -1,35 +1,29 @@
 # divisibility method of a from dev_random:
 # let a', b', c' = a+1, b+1, c+1
-# b' = a' j / k, c' = a' j^2 / k^2
-# assuming gcd(j,k) = 1, k^2 divides a'
-# search through divisors of a' for squares
-# benchmark: 55s
-from number import sieve, factor, divisors
+# geometric sequence b' = a'k/d, c' = a'k^2/d^2 for coprime k,d
+# so d^2 divides a', search a' as multiples of squares
+# P51 pypy3.6 benchmark: 37s
+from number import sieve
 from math import gcd
 
 def S(n):
     s = 0
-    primes = sieve(n)
-    sp = set(primes)
-    squares = set([k**2 for k in range(1, round(n**0.5)+1)])
+    sp = set(sieve(n))
 
-    for a in primes:
-        a1 = a + 1
-        pp = factor(a1, primes)
-        for k2 in divisors(pp):
-            if k2 in squares:
-                k = round(k2 ** 0.5)
-                for j in range(k+1, n):
-                    if gcd(j, k) != 1: continue
-                    b1 = a1 * j // k
-                    c1 = b1 * j // k
-                    if c1 > n: break
-                    b, c = b1 - 1, c1 - 1
-                    if b in sp and c in sp:
-                        #print(f"{j}/{k}", a, b, c)
-                        s += a + b + c
+    for d in range(1, n):
+        for a1 in range(d**2, n, d**2):
+            a = a1 - 1
+            if a not in sp: continue
+            for k in range(d+1, n):
+                b1 = a1 // d * k
+                c1 = b1 // d * k
+                if c1 > n: break
+                if gcd(d, k) > 1: continue
+                b, c = b1 - 1, c1 - 1
+                if b in sp and c in sp:
+                    print(f"{k}/{d}", a, b, c)
+                    s += a + b + c
+
     return s
 
 print(S(10**8))
-
-

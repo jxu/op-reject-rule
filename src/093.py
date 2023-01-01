5d06ec077cefef428973ad37b439f3bb6b511b88
jxu <7989982+jxu@users.noreply.github.com>
2023-01-01 10:04:30 -0500

    93 compute all binary expression trees instead of RPN

--- src/093.py
+++ src/093.py
@@ -1,44 +1,33 @@
-# use RPN to avoid parentheses
-# any expression tree can map to RPN of
-# perm(a, b, c, d) + perm(3 ops)
-from itertools import product, permutations
+# consider all possible binary expression trees
+# subtraction is non-commutative, non-associative
+# (a-b)-(c-d), ((a-b)-c)-d, (a-(b-c))-d, a-((b-c)-d), a-(b-(c-d))
+# assume floating point operations are exact (ex. 3 * (1/3) == 1)
 
-def rpn(expr):
-    s = []
-    for t in expr:
-        if isinstance(t, int):
-            s.append(t)
-        else:
-            y = s.pop()
-            x = s.pop()
-            if t == '+': s.append(x+y)
-            elif t == '-': s.append(x-y)
-            elif t == '*': s.append(x*y)
-            elif t == '/':
-                assert(y != 0)
-                s.append(x/y)
+from itertools import permutations
 
-    assert(len(s) == 1)
-    return s[0]
+def f(x, op, y):
+    if   op == '+': return x + y
+    elif op == '-': return x - y
+    elif op == '*': return x * y
+    elif op == '/': return x / y if y else float("nan")  # avoid div by 0
 
 
 def longest_consecutive(a, b, c, d):
+    ops = ('+', '-', '*', '/')
     targets = set()
-
-    for op_set in product(('+', '-', '*', '/'), repeat=3):
-        for val_set in permutations((a, b, c, d)):
-            expr = val_set + op_set
-            try:
-                r = rpn(expr)
-                # assume float operation returns exact int
-                if r > 0 and r == int(r):   
-                    targets.add(int(r))
-            except: pass
+    for a, b, c, d in permutations((a, b, c, d)):
+        for op0 in ops:
+            for op1 in ops:
+                for op2 in ops:
+                    targets.add(f(f(a, op0, b), op1, f(c, op2, d)))
+                    targets.add(f(f(f(a, op0, b), op1, c), op2, d))
+                    targets.add(f(f(a, op0, f(b, op1, c)), op2, d))
+                    targets.add(f(a, op0, f(f(b, op1, c), op2, d)))
+                    targets.add(f(a, op0, f(b, op1, f(c, op2, d))))
 
     x = 1
     while x in targets:
         x += 1
-
     return x-1
 
 

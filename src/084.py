25caa047f6160a307ca3c934a247bacd093a6550
jxu <7989982+jxu@users.noreply.github.com>
2023-01-03 21:48:18 -0500

    84 use enum instead of list and clean up output

--- src/084.py
+++ src/084.py
@@ -2,12 +2,14 @@
 # shortcut: don't bother taking chest and chance cards in order
 
 from enum import Enum
-from random import randint, sample
+from random import randint
 
-BOARD = ("GO", "A1", "CC1", "A2", "T1", "R1", "B1", "CH1", "B2", "B3",
-      "JAIL", "C1", "U1", "C2", "C3", "R2", "D1", "CC2", "D2", "D3",
-      "FP", "E1", "CH2", "E2", "E3", "R3", "F1", "F2", "U2", "F3",
-      "G2J", "G1", "G2", "CC3", "G3", "R4", "CH3", "H1", "T2", "H2")
+BOARD = Enum("Board",
+      ("GO", "A1", "CC1", "A2", "T1", "R1", "B1", "CH1", "B2", "B3",
+       "JAIL", "C1", "U1", "C2", "C3", "R2", "D1", "CC2", "D2", "D3",
+       "FP", "E1", "CH2", "E2", "E3", "R3", "F1", "F2", "U2", "F3",
+       "G2J", "G1", "G2", "CC3", "G3", "R4", "CH3", "H1", "T2", "H2"),
+      start = 0)
 
 TURNS = 1000000
 counter = [0] * len(BOARD)
@@ -26,74 +28,70 @@ for turn in range(TURNS):
     else:
         doubles = 0
 
-    print(f"dice {d1}+{d2}={d} doubles:{doubles}")
-
+    #print(f"dice {d1}+{d2}={d} doubles:{doubles}")
 
     if doubles == 3:
-        print("3 doubles! go to jail!")
+        #print("3 doubles! go to jail!")
         doubles = 0
-        pos = BOARD.index("JAIL")
+        pos = BOARD.JAIL.value
 
     else:
         # make move
         pos = (pos + d) % len(BOARD)
 
     # G2J square
-    if pos == BOARD.index("G2J"):
-        pos = BOARD.index("JAIL")
+    if pos == BOARD.G2J.value:
+        pos = BOARD.JAIL.value
 
 
 
     # chance card
-    if BOARD[pos].startswith("CH"):
+    if BOARD(pos).name.startswith("CH"):
         card = randint(0, 15)
-        print("chance", card)
+        #print("chance", card)
         if card == 0:
-            pos = BOARD.index("GO")
+            pos = BOARD.GO.value
         elif card == 1:
-            pos = BOARD.index("JAIL")
+            pos = BOARD.JAIL.value
         elif card == 2:
-            pos = BOARD.index("C1")
+            pos = BOARD.C1.value
         elif card == 3:
-            pos = BOARD.index("E3")
+            pos = BOARD.E3.value
         elif card == 4:
-            pos = BOARD.index("H2")
+            pos = BOARD.H2.value
         elif card == 5:
-            pos = BOARD.index("R1")
+            pos = BOARD.R1.value
         elif card in (6, 7):
-            while not BOARD[pos].startswith("R"):
+            while not BOARD(pos).name.startswith("R"):
                 pos = (pos + 1) % len(BOARD)
         elif card == 8:
-            while not BOARD[pos].startswith("U"):
+            while not BOARD(pos).name.startswith("U"):
                 pos = (pos + 1) % len(BOARD)
         elif card == 9:
             pos = (pos - 3) % len(BOARD)
 
 
     # handle community chest card after chance
-    if BOARD[pos].startswith("CC"):
+    if BOARD(pos).name.startswith("CC"):
         card = randint(0, 15)
-        print("chest", card)
+        #print("chest", card)
         if card == 0:
-            pos = BOARD.index("GO")
+            pos = BOARD.GO.value
         elif card == 1:
-            pos = BOARD.index("JAIL")
+            pos = BOARD.JAIL.value
 
 
     # record position after roll
-    print("position", pos, BOARD[pos])
+    #print("position", pos, BOARD(pos).name)
     counter[pos] += 1
-
-print("counter", counter)
 
 most_visited = sorted(range(len(BOARD)),
                       key=lambda i:counter[i],
                       reverse=True)
 
-for i in range(3):
+for i in range(len(BOARD)):
     sq = most_visited[i]
-    print(BOARD[sq], counter[sq])
+    print(BOARD(sq).name, counter[sq])
 
 for i in range(3):
     print(str(most_visited[i]).zfill(2), end='')
-

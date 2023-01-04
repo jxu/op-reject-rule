a157591c49e833594aa185474e4d8df52e2d4d90
jxu <7989982+jxu@users.noreply.github.com>
2023-01-04 17:56:30 -0500

    83 reimplement Dijkstra's algorithm

--- src/083.py
+++ src/083.py
@@ -1,40 +1,43 @@
-def dijkstra(graph, start):
-    """Dijkstra's algorithm using heaps.
+# Dijkstra's algorithm (using simple array instead of priority queue)
+# runtime O(|V|^2 + |E|) = O(n^4)
 
-    Test using g = {0:{1:2}, 1:{0:2, 2:6}, 2:{1:6}}
-    Credit: Janne Karila
-    """
-    from heapq import heappush, heappop
+import csv
+N = 80
+INF = 1000000
 
-    A = [None] * len(graph)
-    queue = [(0, start)]
-    while queue:
-        path_len, v = heappop(queue)
-        if A[v] is None: # v is unvisited
-            A[v] = path_len
-            for w, edge_len in graph[v].items():
-                if A[w] is None:
-                    heappush(queue, (path_len + edge_len, w))
+with open("p082_matrix.txt", 'r') as f:
+    matrix = [list(map(int, row)) for row in csv.reader(f)]
 
-    return A
+graph = {}
+dist = {}
+mark = {}
+for i in range(N):
+    for j in range(N):
+        # graph stored as dict of dicts
+        # {s: {t1:v1, t2:v2, ...}, ...}
+        graph[(i, j)] = {}
+        dist[(i, j)] = INF
+        mark[(i, j)] = False
+        if i > 0:   graph[(i,j)][(i-1,j)] = matrix[i][j]
+        if i < N-1: graph[(i,j)][(i+1,j)] = matrix[i][j]
+        if j > 0:   graph[(i,j)][(i,j-1)] = matrix[i][j]
+        if j < N-1: graph[(i,j)][(i,j+1)] = matrix[i][j]
 
-with open("p083_matrix.txt", 'r') as f:
-    matrix = f.read().splitlines()
-    matrix = [list(map(int, row.split(','))) for row in matrix]
+dist[(0,0)] = 0
 
-def f(i, j):  # flatten
-    return 80*i + j
+for k in range(N*N):
+    v = None  # next vertex to consider
+    # loop through all vertices to find v = argmin_w dist[w]
+    for i in range(N):
+        for j in range(N):
+            w = (i,j)
+            if not mark[w] and (v is None or dist[w] < dist[v]):
+                v = w
 
-g = {}
-for i in range(80):
-    for j in range(80):
-        neighbors = {}
-        if i > 0:   neighbors[f(i-1, j)] = matrix[i-1][j]
-        if i < 79:  neighbors[f(i+1, j)] = matrix[i+1][j]
-        if j > 0:   neighbors[f(i, j-1)] = matrix[i][j-1]
-        if j < 79:  neighbors[f(i, j+1)] = matrix[i][j+1]
+    mark[v] = True  # visit v
+    # loop through edges
+    for target, length in graph[v].items():
+        if dist[v] + length < dist[target]:
+            dist[target] = dist[v] + length
 
-        g[f(i, j)] = neighbors
-
-d = dijkstra(g, 0)
-print(matrix[0][0] + d[-1])
+print(dist[(N-1,N-1)] + matrix[N-1][N-1])

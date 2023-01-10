b8a7d44b6229d4ccb5147db33f48f1e839184812
jxu <7989982+jxu@users.noreply.github.com>
2023-01-10 09:48:48 -0500

    571 brute force

--- src/571.py
+++ src/571.py
@@ -1,3 +1,5 @@
+# brute force: assume base 12 digits only appear once
+# 129s with pypy
 from number import int_to_base
 from itertools import permutations
 
@@ -8,31 +10,33 @@ def pandigital(n, b):
 
 
 def n_super_pandigital(b):
-    l = []
-    for extra_digit in range(b):
-        #digits = list(range(b)) + [extra_digit]
-        digits = range(b)
-        for permutation in permutations(digits):
-            print(permutation)
-            # Create num from digit permutation
-            n = 0
-            base = 1
-            for digit in reversed(permutation):
-                n += digit * base
-                base *= b
+    s = 0
+    found = 0
 
-            valid = True
-            for test_base in range(b-1, 1, -1):
-                if not pandigital(n, test_base):
-                    valid = False
-                    break
+    digits = list(range(b))
+    for permutation in permutations(digits):
+        if permutation[0] == 0:
+            continue
+        # Create num from digit permutation
+        n = 0
+        base_pow = 1
+        for digit in reversed(permutation):
+            n += digit * base_pow
+            base_pow *= b
 
-            if valid:
-                print(n)
-                l.append(n)
+        valid = True
+        for test_base in range(b-1, 1, -1):
+            if not pandigital(n, test_base):
+                valid = False
+                break
 
-            if len(l) == 10:
-                return l
+        if valid:
+            print(permutation, n)
+            s += n
+            found += 1
+
+        if found == 10:
+            return s
 
 
-n_super_pandigital(12)
+print(n_super_pandigital(12))

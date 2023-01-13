93e292c3dc056dfdd3e7e866796a95d15081610f
jxu <7989982+jxu@users.noreply.github.com>
2023-01-12 20:03:16 -0500

    725 explanation

--- src/725.py
+++ src/725.py
@@ -1,3 +1,15 @@
+# DS-number has n-1 digits that sum to d, then digit d somewhere
+# How many n-length DS-numbers sum to 2d?
+# By stars-and-bars, placing n-1 digits that sum to d is (d+n-2 choose n-2)
+# then n places to put digit d, but subtract (n choose 2) because cases with
+# two digit d's (like 3300) are overcounted once.
+
+# Then every DS-number sums to 2d and each digit is treated symmetrically vs
+# all others, so adding up all DS-numbers digit by digit,
+# each digit gets on average (2d / n) * (# DS-numbers)
+# Adding up these numbers digit by digit is the same as multiplying the digit
+# avg by 111... (= (10^n-1)/9)
+
 from number import combination as comb
 M = 10**16
 def S(n):

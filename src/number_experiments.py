1b8b71d7cbbc2baaaa26989bac4e9e36b2ab2082
jxu <7989982+jxu@users.noreply.github.com>
2023-01-15 12:33:39 -0500

    Add CRT recursive experiment

---
+++ src/number_experiments.py
@@ -0,0 +1,35 @@
+def sieve_factor(n):
+    fact = [[] for _ in range(n)]
+    for i in range(2, n):
+        if fact[i] == []:
+            j = i
+            while j <= n:
+                for k in range(j, n, j):
+                    fact[k].append(i)
+                j *= i
+
+    return fact
+
+
+from number import extended_euclidean
+def crt_recursive(rs, mods):
+    """Chinese Remainder Theorem for x = r[i] mod m[i]
+
+    Returns solution (r, m) of x = r mod m, with m = m1*m2*...
+    Exact solution from Wikipedia
+    """
+    print(rs, mods)
+    r0, m0 = rs[0], mods[0]
+    if len(rs) == 1:
+        return r0 % m0, m0
+
+    r1, m1 = rs[1], mods[1]
+    g, n0, n1 = extended_euclidean(m0, m1)
+    r = (r0*n1*m1 + r1*n0*m0) % (m0*m1)
+    m = m0 * m1
+
+    rs = [r] + rs[2:]  # ugly list slicing
+    mods = [m] + mods[2:]
+    return crt_recursive(rs, mods)
+
+assert crt_recursive([2, 3], [3, 5]) == (8, 15)

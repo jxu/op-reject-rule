8d687d04816d3862eee47a15a5ffe8a27f4f12f9
jxu <7989982+jxu@users.noreply.github.com>
2023-01-15 19:08:40 -0500

    743

--- src/743.py
+++ src/743.py
@@ -1,11 +1,19 @@
-from number import comb
+from number import comb, sieve, mul_inv
+M = 1000000007
+
+fact = [1] * (10**8 + 1)
+x = 1
+for i in range(1, 10**8 + 1):
+    fact[i] = (fact[i-1] * i) % M
+
 def A(k, n):
     s = 0
     for i in range(0, k//2+1):
-        t = comb(k,i) * comb(k-i,i) * (2**((k-2*i)*(n//k)))
-        print(t)
-        s += t
+        t = (fact[k] * mul_inv(fact[i] * fact[k-i], M)) % M * \
+            (fact[k-i] * mul_inv(fact[i] * fact[k-2*i], M)) % M * \
+            pow(2, (k-2*i)*(n//k), M)
+        s = (s + t) % M
 
     return s
 
-print(A(4,20))
+print(A(10**8, 10**16))

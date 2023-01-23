8540101598a51ea09606ad9c1cd0d0b6e1403b89
jxu <7989982+jxu@users.noreply.github.com>
2023-01-22 21:06:01 -0500

    710 test placing middle digit

--- src/710.py
+++ src/710.py
@@ -4,43 +4,35 @@ M = 1000000
 
 @memoize
 def f(rem, two_seen):
-
+    s = 0
     if rem == 0:
         if two_seen: return 1
-        else: return 0
+        else:
+            return 0
 
-    s = 0
+    # end building immediately by placing middle digit
+    two_seen_new = rem == 2
+    if two_seen or two_seen_new:
+        s += 1
 
-    for i in range(1, rem+1):
-        #print(rem, two_seen, i)
+    for i in range(1, rem//2+1):
         if i == 2: two_seen_new = True
         else: two_seen_new = two_seen
 
-        s = (s + (f(rem - i, two_seen_new))) % M
+        s = (s + f( rem-2*i, two_seen_new)) % M
 
     return s
 
-#print(f([], 3, False))
 
 def t(n):
-
-    s = 0
-    if n % 2 == 0:
-        for middle in range(0, n+1, 2):
-            two_seen = middle == 2
-            s = (s +  (f((n-middle)//2, two_seen))) % M
-    else:
-        for middle in range(1, n+1, 2):
-            two_seen = middle == 2
-            s = (s +  (f((n-middle)//2, two_seen))) % M
-
-
+    s = f( n, False) % M
     return s
 
-for n in range(42, 1000000):
+
+for n in range(43, 100000):
     tn = t(n)
-    if tn < 1000:
-        print(n, tn)
-    if tn % 1000000 == 0:
+    print(n, tn)
+    if tn == 0:
         print(n)
         break
+

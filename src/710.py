8a189069a1a6f8caed1cbb2580bb245808202cca
jxu <7989982+jxu@users.noreply.github.com>
2023-01-27 17:38:48 -0500

    710

--- src/710.py
+++ src/710.py
@@ -3,36 +3,40 @@ from number import memoize
 M = 1000000
 
 @memoize
+def F(rem, two_seen):
+    if rem < 0: return 0
+    if rem == 0:
+        return 1 if two_seen else 0
+    return f(rem, two_seen) + F(rem-2, two_seen)
+
+
+
+@memoize
 def f(rem, two_seen):
     s = 0
+    if rem < 0: return 0
     if rem == 0:
-        if two_seen: return 1
-        else:
-            return 0
+        return 1 if two_seen else 0
 
     # end building immediately by placing middle digit
-    two_seen_new = rem == 2
-    if two_seen or two_seen_new:
+    if two_seen or rem == 2:
         s += 1
 
-    for i in range(1, rem//2+1):
-        if i == 2: two_seen_new = True
-        else: two_seen_new = two_seen
-
-        s = (s + f( rem-2*i, two_seen_new)) % M
+    s = (s + f(rem - 2, two_seen) + f(rem - 4, True) + F(rem-6, two_seen)) % M
 
     return s
 
 
 def t(n):
-    s = f( n, False) % M
+    s = f(n, False) % M
     return s
 
 
-for n in range(43, 100000):
+for n in range(43, 10000000):
     tn = t(n)
-    print(n, tn)
     if tn == 0:
         print(n)
         break
+    print(n, tn)
+
 

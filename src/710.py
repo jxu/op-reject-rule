3fdd8b2d6cdc5ae9c6cbe88acff3d0e3122d25d7
jxu <7989982+jxu@users.noreply.github.com>
2023-01-27 23:05:41 -0500

    710 explanation

--- src/710.py
+++ src/710.py
@@ -1,24 +1,34 @@
-from number import memoize
+# DP with one parameter solution basically by Reiner Martin
+# Examine first value i of twopal.
+# If i != 2, remove first and last value from smaller twopal.
+# If i = 2, then inner palindrome sums to n-4, and there are 2^((n-4)//2)
+# such palindromes.
+# This gives the recurrence
+# t(n) = 2^{(n-4)//2} + t(n-2) + sum_{i=3..n/2} t(n-2i)
+# = sum_{i=0..n/2-1} t(2i) - t(n-4) + 2^{(n-4)//2}
 
+# Defining partial sums s(n) = sum_{i=0..n/2} t(2i) we get the recurrences
+# t(n) = s(n-2) - t(n-4) + 2^((n-4)//2)
+# s(n) = s(n-2) + t(n)
+
+from functools import lru_cache
 M = 1000000
 
-@memoize
+@lru_cache()
 def s(n):
     if n < 2: return 0
-    return s(n-2) + t(n)
+    return (s(n-2) + t(n)) % M
 
-@memoize
+@lru_cache()
 def t(n):
     if n < 2: return 0
     if n == 2: return 1
     if n == 3: return 0
+    return (s(n-2) - t(n-4) + pow(2, (n-4)//2, M)) % M
 
-    return t(n-2) + 2**((n-4)//2) + sum(t(n-2*i) for i in range(3, n//2+1))
-
-
-for n in range(1, 21):
-    tn = t(n)
-
-    print(n, tn)
+for n in range(43, 10**7):
+    if t(n) == 0:
+        print(n)
+        break
 
 

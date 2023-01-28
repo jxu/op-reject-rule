8e7460b71460e54513a30cb2d088b901da347733
jxu <7989982+jxu@users.noreply.github.com>
2023-01-27 22:17:40 -0500

    710 rewrite recursion

--- src/710.py
+++ src/710.py
@@ -3,40 +3,22 @@ from number import memoize
 M = 1000000
 
 @memoize
-def F(rem, two_seen):
-    if rem < 0: return 0
-    if rem == 0:
-        return 1 if two_seen else 0
-    return f(rem, two_seen) + F(rem-2, two_seen)
-
-
+def s(n):
+    if n < 2: return 0
+    return s(n-2) + t(n)
 
 @memoize
-def f(rem, two_seen):
-    s = 0
-    if rem < 0: return 0
-    if rem == 0:
-        return 1 if two_seen else 0
-
-    # end building immediately by placing middle digit
-    if two_seen or rem == 2:
-        s += 1
-
-    s = (s + f(rem - 2, two_seen) + f(rem - 4, True) + F(rem-6, two_seen)) % M
-
-    return s
-
-
 def t(n):
-    s = f(n, False) % M
-    return s
+    if n < 2: return 0
+    if n == 2: return 1
+    if n == 3: return 0
+
+    return t(n-2) + 2**((n-4)//2) + sum(t(n-2*i) for i in range(3, n//2+1))
 
 
-for n in range(43, 10000000):
+for n in range(1, 21):
     tn = t(n)
-    if tn == 0:
-        print(n)
-        break
+
     print(n, tn)
 
 

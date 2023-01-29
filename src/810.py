bcbdfbb0c5a14bbdaf89d12e936f7358b4337519
jxu <7989982+jxu@users.noreply.github.com>
2023-01-28 19:44:52 -0500

    810 explanation

--- src/810.py
+++ src/810.py
@@ -1,3 +1,7 @@
+# digits(x xp y) = digits(x) + digits(y) - 1
+# Use sieve of Eratosthenes, but stop searching j once xor product is >= 2*n
+# since i xp j will have too many digits
+
 def xor_product(x, y):
     r = 0
     while y:
@@ -5,19 +9,17 @@ def xor_product(x, y):
             r ^= x
         x <<= 1
         y >>= 1
-
     return r
 
-n = 5 * 10**8
-s = [0] * (n)
+n = 1 << 27
+s = [0] * n
 for i in range(2, n):
-
     if s[i] == 0:
         for j in range(2,n):
             xp = xor_product(i,j)
-            if xp >= 2*n: break
+            if xp >= 2*n:
+                break
             if xp < n:
                 s[xp] = 1
 
-
-print([i for i in range(2, n) if s[i] == 0][5*10**6-1])
+print([i for i in range(2, n) if s[i] == 0][5000000-1])

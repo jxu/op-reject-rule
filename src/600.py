40d041b6b7053c98ff57fdc24e85c3f0f2b1d1dc
jxu <7989982+jxu@users.noreply.github.com>
2023-08-30 23:15:03 -0400

    600 Mathematica calculations

--- src/600.py
+++ src/600.py
@@ -1,12 +1,15 @@
+# brute force tester
+
+import sys
 from itertools import product
 
-seen = set()
+N = int(sys.argv[1])
 s = 0  # any hexagon
 s1 = 0  # 60 deg rot
 s2 = 0  # 120 deg rot
 r1 = 0  # reflection thru side
-for a,b,c,d,e,f in product(range(1,8), repeat=6):
-    if a + b + c + d + e + f <= 12 and a + b == d + e and b + c == e + f:
+for a,b,c,d,e,f in product(range(1,N//2), repeat=6):
+    if a + b + c + d + e + f <= N and a + b == d + e and b + c == e + f:
         print(a,b,c,d,e,f)
         s += 1
         if a == c == e: s1 += 1

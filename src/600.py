247202f4de7b5a904ad68a0a17014486411c3775
jxu <7989982+jxu@users.noreply.github.com>
2023-10-05 21:45:01 -0400

    600 python linear recurrence example

--- src/600.py
+++ src/600.py
@@ -1,19 +1,13 @@
-# brute force tester
+# linear recurrence with GF
+# x^6/((1-x)(1-x^2)(1-x^3)(1-x^4)(1-x^6)) = sum a_n x^n
+# Denominator = -x^16 +x^15 +x^14 -2x^11 +x^10 -x^9 +x^7 -x^6 +2x^5 -x^2 -x +1
+# Multiplying by denominator and equating coefficients, we get a recurrence
+n = 55106
+c = [0]*(n+1)
+c[:16] = [0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 5, 6, 10, 12, 17, 21]
+for i in range(16, n+1):
+    c[i] = (c[i-1] + c[i-2] - 2*c[i-5] + c[i-6] - c[i-7] + c[i-9]
+            - c[i-10] + 2*c[i-11] - c[i-14] - c[i-15] + c[i-16])
 
-import sys
-from itertools import product
+print(c[-1])
 
-N = int(sys.argv[1])
-s = 0  # any hexagon
-s1 = 0  # 60 deg rot
-s2 = 0  # 120 deg rot
-r1 = 0  # reflection thru side
-for a,b,c,d,e,f in product(range(1,N//2), repeat=6):
-    if a + b + c + d + e + f <= N and a + b == d + e and b + c == e + f:
-        print(a,b,c,d,e,f)
-        s += 1
-        if a == c == e: s1 += 1
-        if a == b and c == d: s2 += 1
-        if c == e and b == f: r1 += 1
-
-print(s, s1, s2, r1)

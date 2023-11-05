87cddd4eb35bb436b0b660029b2909435b978d80
jxu <7989982+jxu@users.noreply.github.com>
2023-11-05 12:51:47 -0500

    860 rewrite without recursion

--- src/860.py
+++ src/860.py
@@ -1,46 +1,35 @@
-from math import comb, factorial
 from number import comb_mod, mod_inv
-from functools import cache
-import sys
-sys.setrecursionlimit(20000)
 m = 989898989
 
-@cache
-def fact_mod(n, m):
-    assert n >= 0
-    if n == 0: return 1
-    return (n * fact_mod(n-1, m)) % m
 
-@cache
-def fact_inv(n, m):
-    return mod_inv(fact_mod(n, m), m)
+def fact_mod_list(n, m):
+    """Compute [0!, ..., n!] mod m in O(n)."""
+    fact_mod = [1] * (n + 1)
+    for i in range(2, n+1):
+        fact_mod[i] = (i * fact_mod[i-1]) % m
+    return fact_mod
+
+def inv_list(l, m):
+    return list(map(lambda x: mod_inv(x,m), l))
+
 
 def F(n):
-    n //= 2
+    n //= 2  # makes math a little nicer
+    fact_mod = fact_mod_list(2*n, m)
+    fact_inv = inv_list(fact_mod, m)
+
     s = 0
     for i in range (0, n//5+1):
         for j in range((n-5*i) + 1):
             k = (n - 5*i - j)
             assert 2*j + 2*k + 10*i == 2*n
-            #print(i,j,k, 2*j + 2*k + 10*i)
-            s = (s + 2* (fact_mod(2*n, m) *
-                   fact_inv(j, m) *
-                   fact_inv(j+2*i,m) *
-                   fact_inv(k+8*i, m) *
-                   fact_inv(k, m)) % m)
+            r = (1 + (i > 0))
+            for t in (fact_mod[2*n], fact_inv[j], fact_inv[j+2*i],
+                      fact_inv[k], fact_inv[k+8*i]):
+                r = (r * t) % m
+            s = (s + r) % m
+
+    return s
 
 
-    for j in range(n+1):
-        k = n-j
-        s = (s - fact_mod(2*n, m) * (fact_inv(j,m) * fact_inv(k,m))**2) % m
-
-
-    return s % m
-
-for n in range(10000):
-    fact_mod(n,m)
-
-
-print(F(2))
-print(F(10))
 print(F(9898))

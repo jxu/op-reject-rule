1a81040b416e858bd08cb1f05dae1220f1a28fb8
jxu <7989982+jxu@users.noreply.github.com>
2023-11-23 16:15:21 -0500

    862 simplify DP with generator

--- src/862.py
+++ src/862.py
@@ -1,21 +1,21 @@
-from number import prod
+from math import prod
 
 # important to not repeatedly do math.factorial calls
 FACT = (1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800,
         479001600)
 
-def make_hists(N, min_d=0, depth=0):
-    if depth == N:
-        return [[0] * 10]
 
-    new_hists = []
+# trying out generator to save memory
+# also aryanc403 idea: use DP to return sum of c, c^2?
+def make_hists(N, min_d=0):
+    if N == 0:
+        yield [0] * 10
+        return
+
     for d in range(min_d, 10):
-        for hist in make_hists(N, d, depth + 1):
-            new_hist = hist[:]
-            new_hist[d] += 1
-            new_hists.append(new_hist)
-
-    return new_hists
+        for hist in make_hists(N-1, d):
+            hist[d] += 1
+            yield hist
 
 
 def T(N, hist):

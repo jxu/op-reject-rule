69bed5197f450e0fd98a13b09264b501b522e535
jxu <7989982+jxu@users.noreply.github.com>
2024-01-07 21:17:14 -0500

    869

--- src/869.py
+++ src/869.py
@@ -1,31 +1,37 @@
 from number import sieve
 from functools import cache
+from collections import Counter
 
-primes = set(bin(n)[2:] for n in sieve(10))
-print(primes)
-np = len(primes)
+suf = Counter()
+primes = sieve(10**8)
+pb = set()
+for p in primes:
+    s = bin(p)[2:]
+    pb.add(s)
+    for i in range(len(s)+1):
+        suf[s[i:]] += 1
+
+#print(suf)
 
 @cache
-def E(x, more=True):
-    #if len(x) > 3: return 0
-    total = sum(p.endswith(x) and p != x for p in primes)
+def E(x):
+    total = suf[x] - (x in pb)
     #print("x", x, "total", total)
     if total == 0: return 0
 
     x0 = "0" + x
     x1 = "1" + x
-    p0 = int(x0 in primes) / total
-    p0m = (sum(p.endswith(x0) and p != x0 for p in primes)) / total
-    p1 = int(x1 in primes) / total
-    p1m = (sum(p.endswith(x1) and p != x1 for p in primes)) / total
+    p0 = (x0 in pb) / total
+    p0m = (suf[x0] - (x0 in pb)) / total
+    p1 = (x1 in pb) / total
+    p1m = (suf[x1] - (x1 in pb)) / total
 
-
-    #print(p0, p0m, p1, p1m)
 
     e0 = p0 + p0m * (1 + E(x0)) + p1m * E(x1)
     e1 = p1 + p1m * (1 + E(x1)) + p0m * E(x0)
 
-    print(x, p0, p0m, p1, p1m, e0, e1, max(e0, e1), sep='\t\t')
+    #print(x, p0, p0m, p1, p1m, e0, e1, max(e0, e1), sep='\t\t')
+    print(x, e0, e1)
     return max(e0, e1)
 
-print(E(""))
+print(round(E(""),8))

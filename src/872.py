bbdd40dc37ba45500d480d726b8645b5af92ea17
jxu <7989982+jxu@users.noreply.github.com>
2024-01-19 23:26:32 -0500

    872 details and slightly different way

--- src/872.py
+++ src/872.py
@@ -7,16 +7,19 @@
 # So for difference d := n - k, Let n be the current node along the path.
 # If d has bit 2^i set, then n -= 2^i
 def f(n, k):
+    # one-liner replacing d is confusing due to changing n
     d = n - k
-    s = n
-    for i in range(64):
-        if (d >> i) & 1:
-            n -= 1 << i
-            s += n
-    return s
+    return n + sum(n := n - (1 << i) for i in range(64) if (d >> i) & 1)
 
-# one-liner with suspect n assignment expression
-def g(n, k): return n + sum(n := n-(1<<i) for i in range(64) if (n-k)>>i & 1)
+
+def f2(n, k):
+    # without assignment expression, because the nodes along the way are just
+    # the first ith bits of d
+    d = n - k
+    mask = lambda i: (2 << i) - 1
+    # can also use set to avoid conditional, since for the zero bits in d,
+    # d & mask is the same (credit: hkmt)
+    return n + sum(n - (d & mask(i)) for i in range(64) if (d >> i) & 1)
 
 print(f(10**17, 9**17))
-print(g(10**17, 9**17))
+print(f2(10**17, 9**17))

4fce041771c018ccaca2ef10ed2bd59acba9e7ee
jxu <7989982+jxu@users.noreply.github.com>
2024-03-03 21:12:54 -0500

    874 extra knapsack solution

--- src/874.py
+++ src/874.py
@@ -1,10 +1,21 @@
 # We maximize the score with the best-case array [k-1] * n,
 # but this array sum is r = n*(k-1) % k = 6337 above a multiple of k.
-# So, minimize score loss p(k-1) - p(k-1-a_i) for a_i summing to r with DP:
+# So, minimize score loss p(k-1) - p(k-1-a_i) for a_i summing to r
+# This is knapsack with s < n, so DP:
 # m(s) = min(p(k-1) - p(k-1-s), min_{0<i<s} m(i) + m(s-i))
 # This is O(k^2) and the result is max score minus gaps from adjusted primes
 # n * p(k-1) - m(r)
-# Other answers had tropical convolution!?
+
+# Other methods
+# radeye, Icy0: Find which index decrements have the least proportional loss
+# on the sum
+# vikt, hitorare: (DP without assuming final a_i sum)
+# Let d_n[r] be max score for n ints with score r mod k.
+# For m1 + m2 = n, d_n[r] = max_{i+j = r mod k} d_m1[i] + d_m2[j]
+# base case d_1[r] = max_{p(i) = r mod k} p(i)
+# The straightforward solution with m1 = n-1, m2 = 1 takes O(k^2 n) time
+# since the (max,+) convolution is associative, we can use exponentiation by
+# squaring to solve in O(k^2 log n) time
 
 from number import sieve
 primes = sieve(100000)

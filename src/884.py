24b3e675b7e20097949e53c03968b7a6199db374
jxu <7989982+jxu@users.noreply.github.com>
2024-05-09 21:10:08 -0400

    884

--- src/884.py
+++ src/884.py
@@ -1,7 +1,7 @@
 from functools import cache 
 from bisect import bisect
 
-cubes = [i**3 for i in range(100)]
+cubes = [i**3 for i in range(10**6)]
 
 def icbrt(n):
     return cubes[bisect(cubes,n)-1]
@@ -21,12 +21,17 @@ def S(N):
     if N <= 1: return 0
     k = icbrt(N-1)
     q, r = N//k, N%k
+    #print(k, q, r)
 
-    return (q-1)*q//2 + q*S(k) + r*q + S(r) 
+    return k*(q-1)*q//2 + q*S(k) + r*q + S(r)
 
-r = 0
-for i in range(101):
-    print(i,r,S(i))
-    r += d(i)
+# r = 0
+# for i in range(101):
+#     print(i,r,S(i))
+#     r += d(i)
 
-#print(64**(1/3))
+
+for k in range(int(10**(17/3))+1):
+    print(k, S(k**3))
+
+print(S(10**17))

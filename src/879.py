e3adea615f829747170dd6d5730fa4c764f3670b
jxu <7989982+jxu@users.noreply.github.com>
2024-05-14 20:05:06 -0400

    879 3x3 working

--- src/879.py
+++ src/879.py
@@ -1,12 +1,13 @@
 
-LINES = ((1,5,9),)
+LINES = ((1,5,9),(2,5,8),(3,5,7),(4,5,6),
+         (1,4,7),(3,6,9),(1,2,3),(7,8,9))
 
 def bad(s, n):
     if not s: return False
     for line in LINES:
         if (line[1] not in s and  # change to line[1,2] not in s for 4-line
-            (s[-1] == line[0] and n == line[-1]) or
-                (s[-1] == line[-1] and n == line[0])):  # rev seq
+                ((s[-1] == line[0] and n == line[-1]) or
+                (s[-1] == line[-1] and n == line[0]))):  # rev seq
             return True
     return False
 
@@ -21,6 +22,8 @@ def f(rem):
             if not bad(s, n):
                 yield s + [n]
 
-sols = list(f([1,5,9]))
-print(sols)
+nums = list(range(1,10))
+sols = list(filter(lambda x: len(x) >= 2, f(nums)))
+for sol in sols:
+    print(''.join(map(str,sol)))
 print(len(sols))

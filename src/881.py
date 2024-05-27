7f8eb845130ee17fec684f63a5d062d38a6c566a
jxu <7989982+jxu@users.noreply.github.com>
2024-05-27 16:07:40 -0400

    881 3x speedup with linear time poly mult, move Polynomial to extra.py

--- src/881.py
+++ src/881.py
@@ -1,13 +1,9 @@
-# Let n = p1^e1 p2^e2 ... pj^ej.
-# For level k (numbering levels from the bottom up),
+# Let n = p1^e1 p2^e2 ... pj^ej. For level k (numbering levels bottom up),
 # the width of the graph is # solutions to
 # a1 + ... + aj = k, with a1 <= e1, ..., aj <= ej.
 # With generating functions this is simply the coefficient of x^k in
 # (1 + x + x^2 + ... + x^e1) ... (1 + x + x^2 + ... + x^ej)
 # and g(n) is the maximum coefficient of the resulting polynomial.
-# (Here I implemented a simple Polynomial class with polynomial multiplication,
-# but j... suggests optimizing multiplication by 1 + x + x^2 + ...
-# in linear time.)
 
 # To actually find minimal n, I use a Dijkstra-like search of the solution
 # space I came up with while not being able to fall asleep last night.
@@ -18,35 +14,28 @@
 
 from number import sieve
 from heapq import *
+from functools import reduce
 
 primes = sieve(100)
 
-class Polynomial:
-    """Polynomial represented by list of coefficients."""
-    def __init__(self, l):
-        self.coef = l
-        self.deg = len(l) - 1
 
-    def __str__(self):
-        return " + ".join(
-            f"{self.coef[d]} x^{d}" for d in range(self.deg+1))
+# optimization idea by j...
+def mul1s(a, deg1):
+    """Multiply polynomial a by 1 + x + x^2 + ... + x^deg1 in linear time."""
+    dega = len(a) - 1
+    y = a + [0] * deg1
 
-    def __mul__(self, other):
-        p, q = self, other
-        r = [0] * (p.deg + q.deg + 1)
-        for i in range(len(r)):
-            for j in range(i+1):
-                if j <= p.deg and i-j <= q.deg:
-                    r[i] += p.coef[j] * q.coef[i-j]
+    for  i in range(dega + deg1 + 1):
+        y[i] += y[i-1]  # y[-1] is fine
+        if i > deg1: y[i] -= a[i - deg1 - 1]
 
-        return Polynomial(r)
+    return y
 
 
-def g(exponents):
-    q = Polynomial([1])
-    for e in exponents:
-        q *= Polynomial([1] * (e+1))  # math.prod wants SupportsIndex
-    return max(q.coef)
+def g(exps):
+    """Multiply polynomials (1 + x + ... + x^ei)"""
+    return max(reduce(mul1s, exps, [1]))
+
 
 h = [(2, [1])]
 

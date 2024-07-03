b56fb057928e1e679aac683ec0bb0c4d51aa23b0
jxu <7989982+jxu@users.noreply.github.com>
2024-07-03 00:28:55 -0400

    886 try BFS on half paths

--- src/886.py
+++ src/886.py
@@ -1,5 +1,6 @@
 from functools import cache
 from number import sieve
+from collections import Counter
 
 def in_bit(b, i):
     return bool(b & (1 << i))
@@ -19,6 +20,8 @@ def coprime(a, b):
 #print(coprime(6,9))
 #print(coprime(2,3))
 
+
+
 def even_bit_count(n):  # bits 0, 2, 4, 6, ...
     return (n & 0x55555555).bit_count()
 
@@ -26,6 +29,11 @@ def odd_bit_count(n):
     return (n & 0xaaaaaaaa).bit_count()
 
 n = 20
+
+cp_table = [[0]*(n+1) for _ in range(n+1)]
+for a in range(2, n+1):
+    for b in range(2, n+1):
+        cp_table[a][b] = coprime(a, b)
 
 @cache
 def f(seen, k):
@@ -51,18 +59,38 @@ def f(seen, k):
 
     r = 0
     for newk in range(2, n+1):
-        if in_bit(seen, newk) and coprime(k, newk):
+        if in_bit(seen, newk) and cp_table[k][newk]:
             r += f(clear_bit(seen, k), newk)
 
     return r
 
-r = 0
-for k in range(2, n+1):
-    x = ((1 << (n-1)) - 1 ) << 2
-    #print(bin(x))
-    z = f(x, k)
-    #print(k, z)
-    r += z
+# r = 0
+# for k in range(2, n+1):
+#     x = ((1 << (n-1)) - 1 ) << 2
+#     #print(bin(x))
+#     z = f(x, k)
+#     #print(k, z)
+#     r += z
+#
+# print(f.cache_info())
+# print(r)
 
-print(f.cache_info())
-print(r)
+
+# BFS to find all bitsets of length n/2
+seen = [Counter() for _ in range(n//2)]
+for i in range(2, n+1):
+    seen[1][(1 << i, i)] = 1
+
+for d in range(2, n//2):
+    for b,k in seen[d-1]:
+        for i in range(2, n+1):
+            if not in_bit(b, i) and coprime(i, k):
+                newb = set_bit(b,i)
+                seen[d][(newb, i)] += seen[d-1][(b,k)]
+
+    print([(hex(k), v, seen[d][(k, v)]) for k, v in seen[d]])
+    print(len(seen[d]), sum(seen[d].values()))
+
+
+
+

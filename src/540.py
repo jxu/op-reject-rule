e5153958b9d9d162c49f6fae10cc00f7eb8ba628
jxu <7989982+jxu@users.noreply.github.com>
2024-09-17 20:23:24 -0400

    540: brute-force

--- src/540.py
+++ src/540.py
@@ -1,13 +1,23 @@
-# Tree of Pythagorean triples (not for solving problem but for future reference)
+"""Brute force inclusion-exclusion. 2.5 min on laptop"""
 
-def generate(a, b, c, max_c):
-    if c > max_c: return 0
-    #print(a, b, c)
-    s = 1
-    s += generate( a - 2*b + 2*c,  2*a - b + 2*c,  2*a - 2*b + 3*c, max_c)
-    s += generate( a + 2*b + 2*c,  2*a + b + 2*c,  2*a + 2*b + 3*c, max_c)
-    s += generate(-a + 2*b + 2*c, -2*a + b + 2*c, -2*a + 2*b + 3*c, max_c)
+from number import powerset, linear_sieve, linear_sieve_factors, prod
 
-    return s
+def P(N):
+    lp = linear_sieve(int(N**0.5)+1)
 
-print(generate(3, 4, 5, 1000))
+    z = 0
+    for m in range(1, int(N**0.5)+1):
+        nmax = int((N - m**2)**0.5)
+
+        if m % 2: nmax //= 2
+
+        ps = list(powerset(linear_sieve_factors(lp, m).keys()))
+        s = 0 
+        # inclusion-exclusion
+        for factors in ps:
+            s += (-1)**len(factors) * (nmax // prod(factors))
+        z += s
+        
+    return z // 2
+
+print(P(3141592653589793))

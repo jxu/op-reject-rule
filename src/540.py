488967a8087494975f0a80dad3d59615b69efe82
jxu <7989982+jxu@users.noreply.github.com>
2024-09-17 20:33:41 -0400

    540: optimize slightly and comment

--- src/540.py
+++ src/540.py
@@ -2,22 +2,32 @@
 
 from number import powerset, linear_sieve, linear_sieve_factors, prod
 
+# a little more efficient than linear_sieve_factors
+def linear_sieve_distinct_factors(lp, n):
+    primes = []  # simple list instead of dict/set
+    while n > 1:
+        if lp[n] not in primes:
+            primes.append(lp[n])
+        n //= lp[n]
+        
+    return primes
+
+
 def P(N):
     lp = linear_sieve(int(N**0.5)+1)
 
     z = 0
     for m in range(1, int(N**0.5)+1):
         nmax = int((N - m**2)**0.5)
+        if m % 2: nmax //= 2  # m odd => n even
 
-        if m % 2: nmax //= 2
-
-        ps = list(powerset(linear_sieve_factors(lp, m).keys()))
+        ps = powerset(linear_sieve_distinct_factors(lp, m))
         s = 0 
         # inclusion-exclusion
         for factors in ps:
             s += (-1)**len(factors) * (nmax // prod(factors))
         z += s
         
-    return z // 2
+    return z // 2  # symmetry for n < m
 
 print(P(3141592653589793))

42ab283a82e8732987e7b6c7d9b1c994a6158734
jxu <7989982+jxu@users.noreply.github.com>
2024-09-18 20:33:19 -0400

    540: optimization for twice as fast and explanation

--- src/540.py
+++ src/540.py
@@ -1,9 +1,31 @@
-"""Brute force inclusion-exclusion. 2.5 min on laptop"""
+"""Brute force inclusion-exclusion on Euclid's parameterization
 
-from number import powerset, linear_sieve, linear_sieve_factors, prod
+Primitive Pythagorean triples are of the form 
+a = m^2 - n^2, b = 2mn, c = m^2 + n^2 
+with m, n coprime and exactly one of them is even. 
+Nice optimization from thread: WLOG use even m and odd n.
+
+With the constraint m^2 + n^2 = c <= N,
+For each m, nmax = sqrt(N - m^2)
+Let m have distinct prime factors p, q, ...
+By inclusion-exclusion, the count of n coprime to m is 
+nmax - (nmax//p) - (nmax//q) - ... + (nmax//pq) + ... 
+
+Use a linear sieve to get distinct prime factorization for every m, 
+then brute-force inclusion-exclusion on powerset of those primes.
+
+The runtime is about O(W sqrt N), where W is avg 2 ^ omega(nmax) 
+omega(n) = # distinct prime factors of n
+Avg order of sum 2^omega(n) up to sqrt N is O(log(sqrt N)) = O(log n), 
+very small mostly
+
+80 sec on laptop
+"""
+
+from number import powerset, linear_sieve, prod
 
 # a little more efficient than linear_sieve_factors
-def linear_sieve_distinct_factors(lp, n):
+def linear_sieve_primes(lp, n):
     primes = []  # simple list instead of dict/set
     while n > 1:
         if lp[n] not in primes:
@@ -16,18 +38,18 @@ def linear_sieve_distinct_factors(lp, n):
 def P(N):
     lp = linear_sieve(int(N**0.5)+1)
 
-    z = 0
-    for m in range(1, int(N**0.5)+1):
+    s = 0
+    for m in range(2, int(N**0.5)+1, 2):
         nmax = int((N - m**2)**0.5)
-        if m % 2: nmax //= 2  # m odd => n even
-
-        ps = powerset(linear_sieve_distinct_factors(lp, m))
-        s = 0 
+        
+        ps = powerset(linear_sieve_primes(lp, m))
+        sm = 0 
         # inclusion-exclusion
         for factors in ps:
-            s += (-1)**len(factors) * (nmax // prod(factors))
-        z += s
+            sm += (-1)**len(factors) * (nmax // prod(factors))
+        s += sm
         
-    return z // 2  # symmetry for n < m
+    return s
 
+#print(P(10**14))
 print(P(3141592653589793))

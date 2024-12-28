cea4300d246717f747209cc2bc5caed15779c05d
jxu <7989982+jxu@users.noreply.github.com>
2024-12-28 14:10:25 -0500

    918: add memoize

--- src/918.py
+++ src/918.py
@@ -3,7 +3,9 @@ Simple telescoping sum from observing a_{2n} + a_{2n+1} = 3 a_n - 3 a_{n+1}
 S(2n+1) = 1 + 3 a_1 - 3 a_{n+1} = 4 - 3 a_{n+1}
 S(2n) = S(2n+1) - a_{2n+1} 
 """
+from functools import cache  # optional
 
+@cache
 def a(n):
     if n == 1: return 1
     if n % 2 == 0: return 2 * a(n//2)

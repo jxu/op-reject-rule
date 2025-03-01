38c9bbacfd52951dc57dff2170df46df801d9342
jxu <7989982+jxu@users.noreply.github.com>
2025-03-01 16:30:40 -0500

    619: rewrite factoring and gaussian elimination
    still too slow

--- src/619.py
+++ src/619.py
@@ -1,67 +1,70 @@
-# To make more efficient
-from __future__ import division
 import numpy as np
 from number import sieve
 
-np.set_printoptions(threshold=np.nan)
-primes = sieve(234567)
-
-# naive implementation
-def exponent_array(n):
-    arr = np.zeros(len(primes), dtype=int)
-    for i in range(len(primes)):
-        while n % primes[i] == 0:
-            n //= primes[i]
-            arr[i] += 1
-
-        if n == 1: return arr  # Done factoring
-
-    return np.full(len(primes), -1) # Too large prime factor
-
+primes = sieve(55)
 
 # Row-reduce binary matrix
-def binary_rr(m):
-    rows, cols = m.shape
-    l = 0
-    for k in range(min(rows, cols)):
-        print(k)
-        if l >= cols: break
-        # Swap with pivot if m[k,l] is 0
-        if m[k,l] == 0:
-            found_pivot = False
-            while not found_pivot:
-                if l >= cols: break
-                for i in range(k+1, rows):
-                    if m[i,l]:
-                        m[[i,k]] = m[[k,i]]  # Swap rows
-                        found_pivot = True
-                        break
+def binary_rr(A):
+    m, n = A.shape
+    h = k = 0  # pivot row, col
 
-                if not found_pivot: l += 1
+    while h < m and k < n:
+        print("pivot", k)
+        print(A)
+        found_piv = False
+        piv = None
+        for i in range(h, m):
+            if A[i,k]:
+                found_piv = True
+                piv = i
+                break
 
-        if l >= cols: break  # No more rows
+        if not found_piv:
+            k += 1
+        else:
+            A[[h,piv]] = A[[piv,h]]  # swap rows
+            for i in range(h+1, m):
+                f = A[i,k]
+                #print(f)
+                A[i,k] = 0
+                for j in range(k+1, n):
+                    A[i,j] ^= A[h,j] & f
 
-        # For rows below pivot, subtract row
-        for i in range(k+1, rows):
-            if m[i,l]: m[i] ^= m[k]
+            h += 1
+            k += 1
 
-        l += 1
+    return A
 
-    return m
+
+
 
 
 def C(a, b):
     m = np.zeros([b-a+1, len(primes)], dtype=np.int8)
-    for n in range(a, b+1):
-        m[n-a] = exponent_array(n)
+    for i in range(len(primes)):
+        p = primes[i]  # prime and prime powers
+        pp = p
+        while pp <= b:
+            for n in range(pp * (a//pp), b+1, pp):
+                
+                if n < a: continue  # ceil div
+                #print(pp, n)
+                m[n-a,i] += 1
+            pp *= p
+            
 
-    m = m[m[:,0] != -1] % 2
     print(m.shape)
 
+    # remove all 0s rows (no small prime factors), set coef mod 2
+    m = m[~ np.all(m == 0, axis=1)] % 2
+    print("after removing rows")
+    print(m)
+
     rr_m = binary_rr(m)
+    #print(m)
     nullity = int(np.sum(~rr_m.any(axis=1)))  # count 0 rows
     print(nullity)
     return pow(2, nullity, 1000000007) - 1
 
 
-print(C(1000000, 1234567))
+print(C(40, 55))

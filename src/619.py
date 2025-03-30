25e6e3fc069bbc9e840d01d6c42d53e146b88c12
jxu <7989982+jxu@users.noreply.github.com>
2025-03-30 10:29:53 -0400

    619: add notes

--- src/619.py
+++ src/619.py
@@ -1,4 +1,39 @@
+"""The main idea comes from prime exponent vectors mod 2
+Like from Dixon's / quadratic sieve factorization. 
+Seems difficult to come up with the linear algebra on your own.
 
+Let p1, ..., pn be the primes that divide any x in [a,b].
+Factor each x = p1^e1 ... pn^en, then represent it as a vector [e1,...,en].
+Multiplying x's is adding these vectors mod 2, and zero vector represents a
+perfect square.
+Let M be the matrix with vector rows for each x in [a,b]. Any linear combo
+of rows given by M^T y = 0, so the solution is the 2^dim(left null space)
+minus 1 (excluding the empty set).
+
+From here on is sparse Gaussian elimination, because the matrix is too large.
+
+Actually, it turns out this matrix is full rank, so the solution is simply
+(b-a+1) - n for computed n.
+ecnerwala in the solutions describes why:
+
+For which p does there exist k with k^2 p in [a,b]?
+<=> [sqrt(a/p),sqrt(b/p)] contains an integer k.
+A sufficient condition is this interval length is >= 1, i.e.
+sqrt(b/p) - sqrt(a/p) >= 1, <=> (sqrt(b) - sqrt(a))^2 >= p.
+For a = 1000000 and b = 1234567, sqrt(a) = 1000 and sqrt(b) = 1111.1
+So just p <= 111.1^2, in particular this holds for p <= sqrt(b).
+(But not for [a,b] = [40,55])
+
+So for every p <= sqrt(b), there is some x = k^2 p row that only has a 1
+in that p column. Therefore each adds 1 to the rank, and that p can be
+eliminated from other rows.
+
+Then for every p > sqrt(b), for any x as a multiple of p, there can be
+only one multiple of p, and x/p factors as all primes <= sqrt(b) which are
+eliminated out. So these p add 1 to the rank too.
+
+I left the sparse row reduce code anyway because it's more interesting.
+"""
 # - max prime b/2, sparse matrix 217706 x 60415: 4m40s
 # - max pf b-a, sparse 197120 x 20794: 2m25s
 
@@ -69,7 +104,8 @@ def C(a, b):
     for i in range(rows):
 
 
-        if any(primes[pi] > b-a for pi in m[i]):
+        if any(primes[pi] > (b-a) for pi in m[i]):
+            
             continue
         #if len(m[i]) == 0:
         #    continue
@@ -94,4 +130,4 @@ def C(a, b):
 #print(C(5,10))
 #print(C(40,55))
 #print(C(1000,1234))
-print(C(1000000,1234567))
+#print(C(1000000,1234567))

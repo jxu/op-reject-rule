8a81db32d0776b17f224b7141b37ced73bc7471c
jxu <7989982+jxu@users.noreply.github.com>
2025-04-09 18:14:29 -0400

    934: simplify and explanation

--- src/934.py
+++ src/934.py
@@ -1,55 +1,64 @@
+# Idea: consider cases of primes in increasing order
+# if n != 0 mod 2: u(n) = 2
+# else: (n = 0 mod 2)
+#   if n != 0 mod 3: u(n) = 3
+#   else: (n = 0 mod 3, => n = 0 mod 6)
+#     if n != 0 mod 5: u(n) = 5
+#     else: (n = 0 mod 5, => n = 0 mod 30)
+#       if n != 0 mod 7: u(n) = 7
+#       else: (n = 0 mod 7, => n = 0 mod 210)
+#         if n != 0,7 mod 11: u(n) = 11
+#         else: (n = 0,7 mod 11 => n = 0,7*210 mod 11*210)
+#         etc.
+#
+# So we maintain a list of residue classes that are yet unknown u(n)
+# for each prime, the list is extended and modulus is multiplied by p
+# then go through all the residues and check if they are 0, 7, 14, ... mod p
+# if they are, they go in the list. otherwise, u(n) = p, and we add to total
+# p times (how many in the res class occur <= N)
+# 
+# After up to a max prime, for the leftover residues we compute u(n) manually
+# Previously, the max prime is chosen so that the res list fits into memory 
+# while not having too many total n leftover to check. But it's simpler just
+# to extend M until > N and stop computing any bigger residues.
+
 from number import sieve
 
-primes = sieve(1000)
-MAXP = 14
+primes = sieve(100)  # suffices
+MAXP = 15  # adjust to consider as large res list as fits in memory
 
 def U(N):
     s = 0
     M = 1
-    L = [0]
-    for i in range(0, MAXP):
-        p = primes[i]
-        newL = []
+    res = [0]
+    for p in primes[:MAXP]:
+        newres = []
         newM = M * p
-        #print("pM", p, M)
         for j in range(p):
-            for r in L:
-                t = j * M + r
-                if ((t % p) % 7 == 0):
-                    #print(t, "mod", newM, "list")
-                    newL.append(t)
-                else:
-                    #print(t, "mod", newM, "add")
-                    c = ((N) // newM + (((N) % newM) >= t))
-                    #print("count", c)
-                    s += p * c
-        L = newL 
-        M = newM
-
-        print(len(L), "rems mod", M)
-
-    print("leftover about", len(L) * (N // M))
-    # handle remaining cases manually
-    for j in range(N // M + 1):
-        for r in L:
-            t = j * M + r 
-
-            if t == 0 or t > N:
-                continue
-
-            found = False
-            # no need to factor
-            for p in primes[MAXP:]:
-                if (t % p) % 7 > 0:
-                    #print(t, p)
-                    s += p
-                    found = True
+            for r in res:
+                n = j * M + r
+                if n > N:  # res in increasing order
                     break
+                if (n % p) % 7 == 0:
+                    newres.append(n)
+                else:
+                    c = N // newM + (N % newM >= n)
+                    s += p * c
+        
+        res = newres
+        M = newM
+        print(len(res), "rems mod", M)
 
-            assert(found)
-
+    # handle remaining cases individually
+    for n in res:
+        if n == 0: continue
+        # no need to factor, just try primes
+        for p in primes[MAXP:]:
+            if (n % p) % 7 > 0:
+                s += p
+                break
     
-    return s 
+    return s
 
 #print(U(1470))
-print(U(10**17))               
+print(U(10**17))

9c68ad5161934592f5d150f3e112c94d4ef3134f
jxu <7989982+jxu@users.noreply.github.com>
2025-04-18 20:51:15 -0400

    940: explain and clean up a little

--- src/940.py
+++ src/940.py
@@ -1,8 +1,18 @@
+# Find 1-D recurrences:
+# A(m+2,n) = 3 A(m+1,n) + A(m,n)
+# A(m,n+2) = A(m,n+1) + 3 A(m,n)
+# Large values can be computed by exponentiation by squaring.
+# (Idea: use recursion on Fib m,n values instead of exp by squaring)
+#
+# Plan for each A(m,n):
+# Find A(m,0) from A(0,0) and A(1,0)
+# Find A(m,1) from A(0,1) and A(1,1)
+# Find A(m,n) from A(m,0) and A(m,1)
+
 
 import numpy as np
 
 def matrix_powmod(A, k, M):
-    
     r = np.eye(2, dtype=np.int64)
     while k:
         if k % 2:
@@ -17,21 +27,6 @@ M = 1123581313
 
 A = np.array([[3,1],[1,0]], dtype=np.int64)
 B = np.array([[1,3],[1,0]], dtype=np.int64)
-#print(matrix_powmod(A, 1, M))
-
-def a(m,n):
-    # am0 = a(m,0)
-    # am1 = a(m,1)
-    y = np.array([1,0], dtype=np.int64)  # [A(1,0), A(0,0)]
-    z = np.array([2,1], dtype=np.int64)
-    am0 = (matrix_powmod(A, m, M) @ y)[1] % M
-    am1 = (matrix_powmod(A, m, M) @ z)[1] % M
-
-    z1 = np.array([am1, am0], dtype=np.int64)
-
-    amn = (matrix_powmod(B, n, M) @ z1)[1] % M
-
-    return amn
 
 from number import fib_list
 
@@ -40,8 +35,20 @@ F = fib_list(50)
 def S(k):
     s = 0
     for i in range(2, k+1):
+        m = F[i]
+
+        y = np.array([1,0], dtype=np.int64)  # [A(1,0), A(0,0)]
+        z = np.array([2,1], dtype=np.int64)
+        am = matrix_powmod(A, m, M)
+        am0 = (am @ y)[1] % M
+        am1 = (am @ z)[1] % M
+
+        z1 = np.array([am1, am0], dtype=np.int64)
+
         for j in range(2, k+1):
-            s = (s + a(F[i], F[j])) % M
+            n = F[j]
+            amn = (matrix_powmod(B, n, M) @ z1)[1] % M
+            s = (s + amn) % M
     return s
 
 print(S(50))

d31b1dc3aad3dce70ac3576d17c08c118c95b7b9
jxu <7989982+jxu@users.noreply.github.com>
2025-05-05 21:16:12 -0400

    917: Dijkstra experiment

--- src/917.py
+++ src/917.py
@@ -1,36 +1,45 @@
-import numpy as np
+from heapq import *
 
 N = 10
 
 a = [0]
 b =[0]
 s = 102022661
-for n in range(1, 21):
-    print(s)
-    if n % 2:
-        a.append(s)
-    else:
-        b.append(s)
-
+for n in range(1, 2*N+1):
+    if n % 2: a.append(s)
+    else: b.append(s)
     s = (s*s) % 998388889
 
 print(a)
 print(b)
 
-A = np.ones((N+1,N+1), dtype=np.int64) * 10**12
-D = np.full((N+1,N+1), ' ', dtype=bytes)
+def M(i,j):
+    return a[i] + b[j]
 
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        if (i,j) == (1,1):
-            A[i][j] = a[i]+b[j]
-        else:
-            A[i][j] = a[i] + b[j] + min(A[i-1][j], A[i][j-1])
-            if A[i-1][j] < A[i][j-1]:
-                D[i][j] = 'V'
-            else:
-                D[i][j] = '>'
+# test Dijkstra's performance on small graphs
+# priority queue elems are (priority, i, j) for M_ij
+pq = [(M(1,1), 1, 1)]
+visit = set()
+n = 0
 
-print(A)
-print(D)
+while True:
+    n += 1
+    node = heappop(pq)
+    pri, i, j = node
+
+    if (i,j) == (N,N): 
+        print("done", pri)
+        break 
+
+    if i+1 <= N and (i+1,j) not in visit:
+        visit.add((i+1,j))
+        heappush(pq, (pri+M(i+1,j), i+1, j))
+
+    if j+1 <= N and (i,j+1) not in visit:
+        visit.add((i,j+1))
+        heappush(pq, (pri+M(i,j+1), i, j+1))
+
+    print(f"{n} {len(pq)} ({i},{j})")
+    
+
 

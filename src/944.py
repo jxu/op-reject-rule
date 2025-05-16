b2c24699224222891b6355903fef4b49efd1fe2c
jxu <7989982+jxu@users.noreply.github.com>
2025-05-15 22:31:45 -0400

    944: optimize pow mod

--- src/944.py
+++ src/944.py
@@ -5,6 +5,9 @@
 # S(n) = Sum_{k=1..n} k (2^(n-1) - 2^(n - n//k))
 # Use Dirichlet hyperbola method / squareroot trick iterating over n//k. 
 # Time of O(sqrt(n) log n) as there's sqrt(n) iters and exp takes log n.
+# To optimize log n factor out, have faster way to compute 2^x mod p
+
+# Nice optimization: 2^x mod p = 2^(x mod p-1) mod p. pypy 17 -> 12 sec
 
 M = 1234567891
 
@@ -13,7 +16,8 @@ def T(n): return n*(n+1)//2
 def S(n):
     c = int(n**0.5)
     return (T(n) * pow(2, n-1, M)
-        - sum(i * pow(2, n - n//i, M) for i in range(1, n//c + 1)) 
-        - sum((T(n//j) - T(n//(j+1))) * pow(2, n-j, M) for j in range(1, c)))
+        - sum(i * pow(2, (n - n//i) % (M-1), M) for i in range(1, n//c + 1)) 
+        - sum((T(n//j) - T(n//(j+1))) * pow(2, (n-j)%(M-1), M) 
+              for j in range(1, c)))
 
 print(S(10**14) % M)

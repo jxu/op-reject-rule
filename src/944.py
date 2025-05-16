d904d119a324df2c77e5120c7746f48e8e658099
jxu <7989982+jxu@users.noreply.github.com>
2025-05-15 21:25:37 -0400

    944: add short explain

--- src/944.py
+++ src/944.py
@@ -1,11 +1,19 @@
+# for given N and k, #(subsets of {1, ..., N} with k as elivisor)
+# = #(subsets with k and at least one (proper) mult of k)
+# = #(subsets with k) - #(subsets with k and no (proper) mult of k)
+# = 2^(N-1) - 2^(N - N//k)
+# S(n) = Sum_{k=1..n} k (2^(n-1) - 2^(n - n//k))
+# Use Dirichlet hyperbola method / squareroot trick iterating over n//k. 
+# Time of O(sqrt(n) log n) as there's sqrt(n) iters and exp takes log n.
+
 M = 1234567891
 
 def T(n): return n*(n+1)//2
 
 def S(n):
     c = int(n**0.5)
-    return ((T(n) * pow(2, n-1, M)) % M 
-        - sum((i * pow(2, n - n//i, M)) % M for i in range(1, n//c + 1)) 
-        - sum(((T(n//j) - T(n//(j+1))) * pow(2, n-j, M)) % M for j in range(1, c))) % M
+    return (T(n) * pow(2, n-1, M)
+        - sum(i * pow(2, n - n//i, M) for i in range(1, n//c + 1)) 
+        - sum((T(n//j) - T(n//(j+1))) * pow(2, n-j, M) for j in range(1, c)))
 
-print(S(10**14))
+print(S(10**14) % M)

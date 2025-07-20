f454bf496632db674b30f7a11a0bd856cb3600bf
jxu <7989982+jxu@users.noreply.github.com>
2025-07-19 20:44:53 -0400

    952: Try binary search

--- src/952.py
+++ src/952.py
@@ -1,4 +1,4 @@
-from number import sieve, val_fact, mul_order, mod_inv, factor
+from number import sieve, val_fact, mul_order, mod_inv, factor, binary_search
 from collections import Counter
 from math import lcm, gcd
 
@@ -8,25 +8,44 @@ def mul_order_p(a, p, k):
     """ord_p^k a"""
     # Bach and Shallit
     # keep result in factored form
-
+    #print(f"mul order {a} mod {p}^{k}")
     n = p**k
     
-    if gcd(a, n) != 1:
+    if gcd(a, p) != 1:
         raise ValueError
 
     phi_n = (p**(k-1)) * (p - 1)
     factors_phi = Counter({p: k-1}) | factor(p - 1)
-    #print(factors_phi)
-    
+    #sprint(factors_phi)
+
+    t = phi_n
     order = Counter()
     
-    for qi, ei in factors_phi.items():
-        yi = phi_n // (qi ** ei)
+    for pi, ei in factors_phi.items():
+        #print(pi, ei)
+        #t = t // pow(pi, ei)
+
+        # binary search qi?
+        yi = phi_n // (pi ** ei)
+        #print("yi", yi)
+
         xi = pow(a, yi, n)
-        while xi != 1:
-            #print(xi, order)
-            xi = pow(xi, qi, n)
-            order[qi] += 1
+
+        def f(d):
+            return pow(xi, pi**d, n) == 1
+
+        di = binary_search(f, 0, ei) + 1
+
+        #print("order", pi, di)
+        order[pi] = di
+        
+        #while xi != 1:
+        #    #print(order[pi])
+        #    xi = pow(xi, pi, n)
+        #    #t *= pi
+        #    order[pi] += 1
+
+    print("order", order)
 
     return order
     
@@ -37,15 +56,16 @@ def R(a, n):
     
     for p in primes:
         v = val_fact(p, n)
+        #print(p, "val", v)
         o = mul_order_p(a, p, v)
-        print(o)
+        print(p, v, o)
 
         keys = c.keys() | o.keys()
 
         # lcm
         c = Counter({k: max(c[k], o[k]) for k in keys})
     
-        #print(c)
+        #print("final", c)
 
     # get final answer
     r = 1
@@ -56,5 +76,5 @@ def R(a, n):
 
 #print(val_fact(2, 10**7))
 #print(mul_order_p(17, 13, 50))
-#print(mul_order_p(2, 10**9 + 7, ))
-print(R(10**9 + 7, 12))
+#print(R(10**9 + 7, 12))
+print(R(10**9 + 7, 10**4))

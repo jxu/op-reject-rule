1dbda599b03c0f892701cedee1d59b7dcdac44ea
jxu <7989982+jxu@users.noreply.github.com>
2025-07-21 10:24:35 -0400

    952: solve

--- src/952.py
+++ src/952.py
@@ -2,44 +2,19 @@ from number import sieve, val_fact, mul_order, mod_inv, factor, linear_sieve, li
 from collections import Counter
 from math import lcm, gcd, prod
 
-M = 10**9 + 7
-
-def mul_order_pk(a, p, k):
-    """ord_p^k a"""
-    # Bach and Shallit
-    # keep result in factored form
-    #print(f"mul order {a} mod {p}^{k}")
-    n = p**k
-    
-    if gcd(a, p) != 1:
-        raise ValueError
-
-    phi_n = (p**(k-1)) * (p - 1)
-    factors_phi = Counter({p: k-1}) | factor(p - 1)
-
-    order = Counter()
-    
-    for pi, ei in factors_phi.items():
-        yi = phi_n // (pi ** ei)
-        xi = pow(a, yi, n)
-
-        while xi != 1:
-            xi = pow(xi, pi, n)
-            order[pi] += 1
-
-    return order
-
 def factors_prod(c: Counter) -> int:
     return prod(p**e for p, e in c.items())
 
 
-def mul_order_p(a, p, factors_phi):
-    """ord_p a"""
-    # keep result in factored form
+def mul_order_p(a, p, factors_p1):
+    """Computes order of a mod p, given factorization of p-1
 
+    From Bach & Shallit, Algorithmic Number Theory
+    Keeps result in factored form for space efficiency
+    """
     order = Counter()
     
-    for pi, ei in factors_phi.items():
+    for pi, ei in factors_p1.items():
         yi = (p-1) // (pi ** ei)
         xi = pow(a, yi, p)
 
@@ -59,6 +34,8 @@ def lte(a, p, k, factor_p1):
 
     for j in range(2, k+1):
         if p in o:
+            # lifting-the-exponent
+            # if p divides order, then order mod p^(k+1) = p * order mod p^k
             o[p] += k - j + 1
             break
         else:
@@ -70,52 +47,30 @@ def lte(a, p, k, factor_p1):
 
     return o
 
-def R(a, n):
+def R(a, n, M = 10**9 + 7):
     primes = sieve(n)
     c = Counter()
     lp = linear_sieve(n) 
     
     for p in primes:
-        # test certain p
         v = val_fact(p, n)
-        #print(p, "val", v)
-
-        #w = 1
-
-        # somehow use for odd p, ord_p^k a = p * ord_p^(k-1) a
-        # p = 2 case? 
-        # if p | ord_p^(k-1) a
-        #for w in range(1, 100):
-        #    print(w, mul_order_p(a,p,w))
-
-        if p == 2:
-            # by induction
-            assert (a % 32) == 7 and v >= 4
+        if p == 2:  
+            # special case for this a and k >= 4: 
+            # order mod 2^k = 2^(k-3), by induction or LTE
             o = Counter({2: v-3})
         
         else:
             o = lte(a, p, v, linear_sieve_factors(lp, p-1))
        
-        if p % 100 == 1:
-            print(p, v, o)
-
-        keys = c.keys() | o.keys()
-
         # lcm
-        c = Counter({k: max(c[k], o[k]) for k in keys})
+        for k in o:
+            c[k] = max(c[k], o[k])
     
-        #print("final", c)
-
-        #break  # TESTING p = 2
-
-    # get final answer
+    # get final product
     r = 1
     for p, e in c.items():
         r = (r * pow(p, e, M)) % M
 
     return r
 
-#print(val_fact(2, 10**7))
-#print(mul_order_p(10**9+7, 2, 9995))
-#print(R(10**9 + 7, 12))
 print(R(10**9 + 7, 10**7))

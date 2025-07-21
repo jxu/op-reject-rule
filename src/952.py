c6e2378359b8dbe23f9f02c6294919bd38b71fae
jxu <7989982+jxu@users.noreply.github.com>
2025-07-21 18:40:12 -0400

    952: clean up mul order functions

--- src/952.py
+++ src/952.py
@@ -1,72 +1,84 @@
-from number import sieve, val_fact, mul_order, mod_inv, factor, linear_sieve, linear_sieve_factors
+from number import sieve, val_fact, linear_sieve, linear_sieve_factors
 from collections import Counter
-from math import lcm, gcd, prod
-
-def factors_prod(c: Counter) -> int:
-    return prod(p**e for p, e in c.items())
 
 
-def mul_order_p(a, p, factors_p1):
+def mul_order_p(a: int, p: int, factors_p1: Counter[int]) -> \
+    tuple[int, Counter[int]]:
     """Computes order of a mod p, given factorization of p-1
 
-    From Bach & Shallit, Algorithmic Number Theory
+    From Handbook of Applied Cryptography
     Keeps result in factored form for space efficiency
     """
     order = Counter()
+    t = p - 1
     
     for pi, ei in factors_p1.items():
-        yi = (p-1) // (pi ** ei)
-        xi = pow(a, yi, p)
+        t //= (pi ** ei)
+        a1 = pow(a, t, p)
 
-        while xi != 1:
-            xi = pow(xi, pi, p)
+        while a1 != 1:
+            a1 = pow(a1, pi, p)
+            t *= pi
             order[pi] += 1
 
-    return order
+    return t, order
 
 
-def lte(a, p, k, factor_p1):
+def mul_order_pk(a: int, p: int, k: int, factor_p1: Counter[int]) -> \
+    Counter[int]:
+    """Compute order of a mod p^k for large k by trying small powers
+
+    p must be odd. Keeps order in factored form.
+
+    Idea is hope we can lift the exponent:
+    ord_{p^k}(a) = ord_{p^{k-1}}(a) or p times that
+
+    Usefully, if p | ord_{p^k}(a), then ord_{p^{k+1}}(a) = p * ord_{p^k}(a)
+    and we can apply this all the way up the powers
+
+    https://math.stackexchange.com/a/4751948
+    """
     if p < 3: raise ValueError
 
-    j = 1
-    o = mul_order_p(a, p, factor_p1)
-    on = factors_prod(o)
+    t, o = mul_order_p(a, p, factor_p1)
 
     for j in range(2, k+1):
         if p in o:
             # lifting-the-exponent
             # if p divides order, then order mod p^(k+1) = p * order mod p^k
+            # apply this all the way from j to k
             o[p] += k - j + 1
             break
         else:
-            if pow(a, on, p**j) == 1:
-                pass
-            else:
+            if pow(a, t, p**j) != 1:
+                # t not sufficient, so must be p*t 
                 o[p] += 1
-                on *= p
-
+    
     return o
 
+
 def R(a, n, M = 10**9 + 7):
-    primes = sieve(n)
+    primes = sieve(n)  # primes in n!
     c = Counter()
     lp = linear_sieve(n) 
-    
+   
+    # compute order for p^k making up n!
     for p in primes:
-        v = val_fact(p, n)
+        v = val_fact(p, n)  # power of p in n!
         if p == 2:  
             # special case for this a and k >= 4: 
             # order mod 2^k = 2^(k-3), by induction or LTE
             o = Counter({2: v-3})
         
         else:
-            o = lte(a, p, v, linear_sieve_factors(lp, p-1))
+            lf = linear_sieve_factors(lp, p-1)
+            o = mul_order_pk(a, p, v, lf)
        
-        # lcm
+        # lcm property: order mod mn = lcm(order mod m, order mod n)
         for k in o:
             c[k] = max(c[k], o[k])
     
-    # get final product
+    # compute final product
     r = 1
     for p, e in c.items():
         r = (r * pow(p, e, M)) % M
